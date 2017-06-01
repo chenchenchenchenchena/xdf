60222974b56069d583b8c92f98cd90a371b4c27e
v_高文生 <v_gaowensheng@xdf.cn>
2017-06-01 14:43:15 +0800

    修改路径

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -3,8 +3,6 @@ $(function(){
 	    url: 'http://dt.staff.xdf.cn/xdfdtmanager/login/login.do',
 	    type: 'post',
 	    dataType: 'json',
-	    asyns:false,
-	    data: JSON.stringify(o),
 	    success:function(e){
 	        console.log(e)
 	        location.href = e.url
