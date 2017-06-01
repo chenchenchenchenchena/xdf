ff8f88e9b1660bdcc275f2b2645cb1134452a91c
v_高文生 <v_gaowensheng@xdf.cn>
2017-06-01 11:46:58 +0800

    修改配置项

--- src/main/webapp/js/scanCode.js
+++ src/main/webapp/js/scanCode.js
@@ -12,7 +12,7 @@ $.ajax({
         console.log(p.timestamp)
         //配置
         wx.config({
-            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: o.appid,   // 必填，公众号的唯一标识  
             timestamp: p.timestamp,   // 必填，生成签名的时间戳
             nonceStr: p.nonceStr,   // 必填，生成签名的随机串
@@ -24,7 +24,7 @@ $.ajax({
             wx.checkJsApi({
                 jsApiList:['scanQRCode'],
                 success:function(res){
-                    alert(0000)
+                    alert('0123498646')
                 }
             })
         });
