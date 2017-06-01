0bc594ed45b502b5e5caed5afd531c9aebd22b70
v_高文生 <v_gaowensheng@xdf.cn>
2017-06-01 16:38:58 +0800

    提交

--- src/main/webapp/js/scanCode.js
+++ src/main/webapp/js/scanCode.js
@@ -9,9 +9,9 @@ $.ajax({
     asyns:false,
     data: JSON.stringify(Wxconfig),
     success:function(e){
-        console.log(e)
+        // console.log(e)
         Wxconfig_h = e;
-        console.log(Wxconfig_h.timestamp);
+        // console.log(Wxconfig_h.timestamp);
         //配置
         wx.config({
             debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
