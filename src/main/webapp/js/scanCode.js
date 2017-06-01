cb3fe9853ea869a404f4a8cfc75d3196db977c9a
v_高文生 <v_gaowensheng@xdf.cn>
2017-06-01 16:27:44 +0800

    提交

--- src/main/webapp/js/scanCode.js
+++ src/main/webapp/js/scanCode.js
@@ -1,45 +1,24 @@
-
 $(function(){
-
-var o =  {"url" : location.href,"appid" : "wx559791e14e9ce521","secret": "baa4373d5a8750c69b9d1655a2e31370"}
-var p;
-            // console.log(sessionStorage.e2state);
-            console.log( {'code':location.search.substring(location.search.indexOf('code')+5,location.search.indexOf('&')),
-            'state':location.search.substring(location.search.indexOf('state')+6,location.search.length),
-            'e2State':sessionStorage.e2state})
-            var aa  = {
-            'code':location.search.substring(location.search.indexOf('code')+5,location.search.indexOf('&')),
-            'state':location.search.substring(location.search.indexOf('state')+6,location.search.length),
-            'e2State':sessionStorage.e2state
-        }
-$.ajax({
-        url: 'http://dt.staff.xdf.cn/xdfdtmanager/login/doLogin.do',
-        type: 'post',
-        asyns:false,
-        dataType: 'json',
-        data:JSON.stringify(aa),
-        success:function(e){
-            console.log(e)
-            // alert(sessionStorage.e2state)
-        }
-    })
+// 配置微信启动项
+var Wxconfig =  {"url" : location.href,"appid" : "wx559791e14e9ce521","secret": "baa4373d5a8750c69b9d1655a2e31370"}
+var Wxconfig_h;
 $.ajax({
     url: 'http://dt.staff.xdf.cn/xdfdtmanager/wechatSignature/getWeChatSignature.do',
     type: 'post',
     dataType: 'json',
     asyns:false,
-    data: JSON.stringify(o),
+    data: JSON.stringify(Wxconfig),
     success:function(e){
         console.log(e)
-         p = e;
-        console.log(p.timestamp)
+        Wxconfig_h = e;
+        console.log(Wxconfig_h.timestamp)
         //配置
         wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId: o.appid,   // 必填，公众号的唯一标识  
-            timestamp: p.timestamp,   // 必填，生成签名的时间戳
-            nonceStr: p.nonceStr,   // 必填，生成签名的随机串
-            signature: p.signature, // 必填，签名
+            appId: Wxconfig.appid,   // 必填，公众号的唯一标识  
+            timestamp: Wxconfig_h.timestamp,   // 必填，生成签名的时间戳
+            nonceStr: Wxconfig_h.nonceStr,   // 必填，生成签名的随机串
+            signature: Wxconfig_h.signature, // 必填，签名
             jsApiList: ["checkJsApi","scanQRCode"]
         });
         //调用成功
@@ -53,37 +32,39 @@ $.ajax({
         });
         //调用失败
         wx.error(function(res){
-            // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
             alert("出错了"+res.errMsg)
         });
 
     }
 })
 
-
+// 绑定点击事件
 $("#scanQRCode").click(function() {
-            wx.scanQRCode({
-                // 默认为0，扫描结果由微信处理，1则直接返回扫描结果
-                needResult : 1,
-                desc : 'scanQRCode desc',
-                success : function(res) {
-                    //扫码后获取结果参数赋值给Input
-                    $('body').append('<h1>'+res.resultStr+'</h1>')
-                }
-            });
-        });
-// $("#scanQRCode").click(function(){
-//     // alert(0)
-// 	wx.scanQRCode({
-// 	    needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-// 	    scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
-// 	    success: function (res) {
-// 	    	var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-// 		}
-// 	});
-// })
+    wx.scanQRCode({
+        needResult : 1,  // 默认为0，扫描结果由微信处理，1则直接返回扫描结果
+        desc : 'scanQRCode desc',
+        success : function(res) {
+            $('body').append('<h1>'+res.resultStr+'</h1>')
+        }
+    });
+});
 
- 
+// 请求教师信息
+console.log( {'code':location.search.substring(location.search.indexOf('code')+5,location.search.indexOf('&')),'state':location.search.substring(location.search.indexOf('state')+6,location.search.length),'e2State':sessionStorage.e2state})
+var tname_config  = {'code':location.search.substring(location.search.indexOf('code')+5,location.search.indexOf('&')),'state':location.search.substring(location.search.indexOf('state')+6,location.search.length),'e2State':sessionStorage.e2state}
+$.ajax({
+        url: 'http://dt.staff.xdf.cn/xdfdtmanager/login/doLogin.do',
+        type: 'post',
+        asyns:false,
+        dataType: 'json',
+        data:JSON.stringify(tname_config),
+        success:function(e){
+            console.log(e)
+        }
+})
+
+
+// 获取信息
 var jsona ={"Method":"syncStudentsData","studentsData":{"acceptorId":"0035005A000C514D413535111","startDate":"2017-03-28 17:39:30","endDate":"2017-03-28 17:39:30","saveType":"new","schoolId":"002","schoolName":"机构校区002","classId":"001","className":"演示版本教室一","teacherId":"002","teacherName":"演示版本辅导老师","masterTeacherId":"aode@xdf.cn","masterTeacherName":" 阿城市市场","lessonNo": "1","devInfos": [{"studentName":"student A21","studentId":"A21","studentUserId":"UserId"}]}}
 var jsonb = JSON.stringify(jsona)
 // $.ajax({
