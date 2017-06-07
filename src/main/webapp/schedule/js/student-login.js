a9e5ca1052525cc50f3650ba2951d25ed199dba9
高文生 <v_gaowensheng@xdf.cn>
2017-06-07 17:52:43 +0800

    修改公共部分文件

--- src/main/webapp/schedule/js/student-login.js
+++ src/main/webapp/schedule/js/student-login.js
@@ -1,7 +1,6 @@
 $(function(){
 	$(".inputBox p").eq(1).hide();
 	$(".studentTitle li").tap(function(){
-		/*alert($(this).index());*/
 		$(this).addClass("show").siblings().removeClass("show");
 		$(".inputBox p").eq($(this).index()).show().siblings("p").hide();
 	})
@@ -9,4 +8,51 @@ $(function(){
 		$(this).css("background","#ff6a6a");
 		$(this).html("解除关联");
 	})
+
+
+
+// 教师登录
+$('.enter').click(function(){
+	var  cbconfig = {'callbackFlag':'teacherWX'};
+	$.ajax({
+	    url: 'http://dt.staff.xdf.cn/xdfdtmanager/e2Login/login.do',
+	    type: 'post',
+	    dataType: 'json',
+	    data:JSON.stringify(cbconfig),
+	    success:function(e){
+	        console.log(e)
+	        location.href = e.url
+	        sessionStorage.e2state = e.e2state
+	        // alert(sessionStorage.e2state)
+	    }
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
