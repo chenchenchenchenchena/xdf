ea5f6740d054bb4c78e3265a6df6d93fdf9f9379
高文生 <v_gaowensheng@xdf.cn>
2017-06-08 13:44:10 +0800

    提交

--- src/main/webapp/schedule/js/login_s.js
+++ src/main/webapp/schedule/js/login_s.js
@@ -1,22 +1,18 @@
 $(function(){
-	$(".inputBox p").eq(1).hide();
+	// tab切换
 	$(".studentTitle li").tap(function(){
 		$(this).addClass("show").siblings().removeClass("show");
 		$(".inputBox p").eq($(this).index()).show().siblings("p").hide();
 	})
-	$(".deterAss").tap(function(){
-		$(this).css("background","#ff6a6a");
-		$(this).html("解除关联");
-	})
 
+	var wxnumber = {'email':'','wechatId':Wxid}
+	ajax_S(url.t_wxmo,wxnumber,Wxtea)//ajax请求
 
-
-
-
-
-
-
-
+	function Wxtea(e){
+		if(e.data!='goE2'){
+			location.href = 'schedule_t.html'
+		}
+	}
 
 
 
---
+++ src/main/webapp/schedule/js/login_t.js
@@ -0,0 +1,25 @@
+// var bindingtea = {'email':$('.name_ema').html(),'wechatId':Wxid}
+var bindingtea = {'email':'caoxuefeng@xdf.cn','wechatId':Wxid}
+
+function teamore(e){
+			if(e.result==true){
+				$('.name_s').html(e.userName);
+				$('.name_ema').html(e.email);
+				ajax_S(url.t_wxmo,bindingtea,binding)//ajax请求
+			}else{
+				etlogin('teacherWX')
+			}
+		}//e2登陆完成 stat查询老师信息
+
+
+
+function binding(e){
+	var teacontent = JSON.parse(e.data)
+	sessionStorage.teacherName = teacontent.teacherName
+	sessionStorage.teacherNo = teacontent.teacherNo
+	sessionStorage.teacherEmail = teacontent.teacherEmail
+	sessionStorage.mobile = teacontent.mobile
+	sessionStorage.schoolId = teacontent.schoolId
+}//获取老师绑定信息
+
+ajax_S(url.t_more,calbac,teamore)//ajax请求
