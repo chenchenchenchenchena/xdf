b5a305edaf5f0dfa271e9ffb8135ea626ed7f816
高文生 <v_gaowensheng@xdf.cn>
2017-06-08 17:22:04 +0800

    提交

--- src/main/webapp/common/js/ajax.js
+++ src/main/webapp/common/js/ajax.js
@@ -5,8 +5,9 @@ var url = {
     't_wxmo':'http://dt.staff.xdf.cn/xdfdtmanager/teacherBind/queryTeacherInfo.do',   //学生登录页  通过微信查询是否登录过
     't_siot':'http://dt.staff.xdf.cn/xdfdtmanager/teacherBind/unbindTeacherInfo.do',   //解绑
     's_seac':'http://dt.staff.xdf.cn/xdfdtmanager/studentBind/queryStudentInfo.do',  //学员号查询
-    's_bind':'http://dt.staff.xdf.cn/xdfdtmanager/studentBind/bindWechatandStudent.do',   //学员号查询
-    's_nobd':'http://dt.staff.xdf.cn/xdfdtmanager/studentBind/unbindStudentInfo.do'   //学员号查询
+    's_bind':'http://dt.staff.xdf.cn/xdfdtmanager/studentBind/bindWechatandStudent.do',   //学员号绑定微信
+    's_nobd':'http://dt.staff.xdf.cn/xdfdtmanager/studentBind/unbindStudentInfo.do',  //学员号解绑微信
+    's_nafu':'http://dt.staff.xdf.cn/xdfdtmanager/studentBind/queryStuInfoByNameMobile.do'   //姓名手机号查询
 }
 
 var Wxid = 'wechatid123456'
--- src/main/webapp/schedule/js/login_s.js
+++ src/main/webapp/schedule/js/login_s.js
@@ -1,9 +1,15 @@
 $(function(){
 	// tab切换
-	// $(".studentTitle li").tap(function(){
-	// 	$(this).addClass("show").siblings().removeClass("show");
-	// 	$(".inputBox p").eq($(this).index()).show().siblings("p").hide();
-	// })
+	$(".studentTitle li").on('touchend',function(){
+		if($(this).index()==1){
+			$('.card').hide()
+		}else{
+			$('.card').show()
+		}
+		$(this).addClass("show").siblings().removeClass("show");
+		$(".inputBox p").eq($(this).index()).show().siblings("p").hide();
+	})
+
 	var wxnumber = {'email':'','wechatId':Wxid}
 	
 	//判断教师是否绑定
@@ -13,17 +19,23 @@ $(function(){
 		}
 	}
 
-	//查询学生信息
+
+	//查询学生信息  学员号查询
 	function stusea(e){
+		var num = /^\w+$/
 		if($('.studentLogin input').val()==''){
 			layer.msg('请先输入学员号');
+			return false;
+		}
+		if(!num.test($('.studentLogin input').val())){
+			layer.msg('请输入正确格式学员号');
 			return false;
 		}
 		console.log(e)
 		if(e.result==true){
 			$('.card').hide()
 			$('.search').show()
-			$('.stuname').html(e.studentName)
+			$('.stuname').html(e.data.studentName)
 			$('.stutel').html('')
 			sessionStorage.stuNum = $('.studentLogin input').val()
 			if(e.relatedState==0){
@@ -53,26 +65,64 @@ $(function(){
 	//关联点击
 	$('.deterAss').click(function(){
 		var stumore  = {'StudentCode':$('.stunum').val(),'wechatId':Wxid}
-		// 关联
+		// 关联点击
 		if($(this).html()=='立即关联'){
 			ajax_S(url.s_bind,stumore,s_bind)
 		}
 		//解绑
 		else{
-			ajax_S(url.s_nobd,stumore,s_bind)
+			ajax_S(url.s_nobd,stunummore,s_bind)
 		}
 	})
 	ajax_S(url.t_wxmo,wxnumber,Wxtea)//ajax请求
-	// ajax_S(url.s_seac,stumore,stusea)//ajax请求
-
-	
 
 
 
 
+	// 姓名手机号查询
+	function name_se(e){
+		console.log(e.data.studentNo=="[]")
+		var tel = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/; 
+		var name = /^[\u4e00-\u9fa5]{2,4}$/
+		if($('.phoneNumber').val()==''||$('.stname').val()==''){
+			layer.msg('请先将信息填写完整');
+			return false;
+		}
+		if(!tel.test($('.phoneNumber').val())){
+			layer.msg('请输入正确手机号');
+			return false;
+		}
+		if(!name.test($('.stname').val())){
+			layer.msg('请输入正确格式名字');
+			return false;
+		}
+		if(e.data.studentNo=="[]"){
+			layer.msg('没有查到相关信息');
+			return false;
+		}
+		var  studentNo =  e.data.studentNo
+		var  studentNt = studentNo.substring(1,studentNo.length-1)
+		var  arr = studentNo.split('"')
+		var  num = 0;
+		for(var i = 0;i<arr.length;i++){
+				$('.searchTwo').show()
+			if(arr[i]=="["||arr[i]=="]"){
+				// arr.splice(i,1)
+			}else{
+				num++
+				$('.stuNum').append('<li><span>学员号'+num+':</span><span>'+arr[i]+'</span><button>确认关联</button></li>')
+			}
+		}
+	}
 
 
 
+
+
+$('.tel_log').click(function(){
+	var stuname = {'name':$('.stname').val(),'mobile':$('.phoneNumber').val()}	
+	ajax_S(url.s_nafu,stuname,name_se)//ajax请求
+})
 
 
 
