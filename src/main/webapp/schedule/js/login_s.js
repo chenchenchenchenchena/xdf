6b0a47ddeba2bcd5ca762cd505ea2e7f26d7cf0f
高文生 <v_gaowensheng@xdf.cn>
2017-06-09 11:13:49 +0800

    提交

--- src/main/webapp/schedule/js/login_s.js
+++ src/main/webapp/schedule/js/login_s.js
@@ -12,7 +12,7 @@ $(function(){
 		$(this).addClass("show").siblings().removeClass("show");
 		$(".inputBox p").eq($(this).index()).show().siblings("p").hide();
 	})
-	alert(sessionStorage.Wxid)
+	alert(sessionStorage.openid)
 	var wxnumber = {'email':'','wechatId':Wxid}
 	
 	//判断教师是否绑定
