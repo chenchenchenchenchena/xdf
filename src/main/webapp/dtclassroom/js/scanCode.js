939d86318817b735b61857bf4ca4a9b42db94a8b
v_高文生 <v_gaowensheng@xdf.cn>
2017-06-14 11:18:52 +0800

    Replace scanCode.js

--- src/main/webapp/dtclassroom/js/scanCode.js
+++ src/main/webapp/dtclassroom/js/scanCode.js
@@ -1,194 +1,29 @@
-$(function(){
-// 配置微信启动项
-var Wxconfig =  {"url" : location.href,"appid" : "wx559791e14e9ce521","secret": "baa4373d5a8750c69b9d1655a2e31370"};
-var Wxconfig_h;
-$.ajax({
-    url: 'http://dt.staff.xdf.cn/xdfdtmanager/wechatSignature/getWeChatSignature.do',
-    type: 'post',
-    dataType: 'json',
-    asyns:false,
-    data: JSON.stringify(Wxconfig),
-    success:function(e){
-        // console.log(e)
-        Wxconfig_h = e;
-        // console.log(Wxconfig_h.timestamp);
-        //配置
-        wx.config({
-            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId: Wxconfig.appid,   // 必填，公众号的唯一标识  
-            timestamp: Wxconfig_h.timestamp,   // 必填，生成签名的时间戳
-            nonceStr: Wxconfig_h.nonceStr,   // 必填，生成签名的随机串
-            signature: Wxconfig_h.signature, // 必填，签名
-            jsApiList: ["checkJsApi","scanQRCode"]
-        });
-        //调用成功
-        wx.ready(function(){
-            wx.checkJsApi({
-                jsApiList:["checkJsApi",'scanQRCode'],
-                success:function(res){
-                }
-            })
-        });
-        //调用失败
-        wx.error(function(res){
-            alert("出错了"+res.errMsg)
-        });
-
-    }
-});
-
-// 绑定点击事件
-$("#scanQRCode").click(function() {
-    wx.scanQRCode({
-        needResult : 1,  // 默认为0，扫描结果由微信处理，1则直接返回扫描结果
-        desc : 'scanQRCode desc',
-        success : function(res) {
-            $('.Wxid').val(res.resultStr)
-            more_s()
-        }
-    });
-});
-// alert(sessionStorage.e2state)
-// 请求用户登录信息
-console.log( {'code':location.search.substring(location.search.indexOf('code')+5,location.search.indexOf('&')),'state':location.search.substring(location.search.indexOf('state')+6,location.search.length),'e2State':localStorage.e2state})
-var tname_config  = {'code':location.search.substring(location.search.indexOf('code')+5,location.search.indexOf('&')),'state':location.search.substring(location.search.indexOf('state')+6,location.search.length),'e2State':localStorage.e2state}
-$.ajax({
-        url: 'http://dt.staff.xdf.cn/xdfdtmanager/e2Login/doLogin.do',
-        type: 'post',
-        asyns:false,
-        dataType: 'json',
-        data:JSON.stringify(tname_config),
-        success:function(e){
-            // if(e.result==false){
-            //     $('.sucs').hide();
-            //     $('.erro').show();
-            //     $('.erro i').html(e.message);
-            // }else{
-            //     $('.erro').hide();
-                $('.sucs').show();
-            //     $('.sucs i').html(e.message);
-            // }
-        }
-});
-
-// more_s()
-
-//查询用户详细信息
-function more_s(){
-var  teacherid = 'caoxuefeng@xdf.cn'
-var  studata   = [],stumodata = [];
-var teachmore = {};
-$.ajax({
-    url: 'http://dt.staff.xdf.cn/xdfdtmanager/studentDataController/getClassDatas.do',
-    type: 'POST',
-    dataType: 'JSON',
-    asyns:false,
-    data:{'email':teacherid},
-    success:function(e){   
-        // alert(0)
-        var more = JSON.parse(e);
-        console.log(more)
-        var class_s = more.data;
-        // for(var i = 0;i<class_s.length;i++){
-            var stumore = more.data.Students;
-            var teamore = more.data.teachers;
-            for(var k = 0;k<stumore.length;k++){
-                stumodata.push({
-                    'studentName' :stumore[k].StudentName,
-                    'studentId':stumore[k].Id,
-                    'studentUserId':stumore[k].CardCode ,
-                });
-            }
-            if(teamore.length==1){
-                teachmore = {
-                    'masterTeacherId':teamore[0].Id,
-                    'masterTeacherName':teamore[0].sName,
-                    'teacherId':teacherid,
-                    'teacherName':teamore[0].sName
-                }
-            }else{
-                for(var j = 0;j<teamore.length;j++){
-                    if(teamore[j].sEmail ==teacherid){
-                    if(j==1){
-                            teachmore = {
-                            'masterTeacherId':teamore[j-1].sEmail,
-                            'masterTeacherName':teamore[j-1].sName,
-                            'teacherId':teacherid,
-                            'teacherName':teamore[j]. sName
-                        }
-                    }else{
-                        teachmore = {
-                            'masterTeacherId':teamore[j+1].sEmail,
-                            'masterTeacherName':teamore[j+1].sName,
-                            'teacherId':teacherid,
-                            'teacherName':teamore[j].sName
-                        }
-                    }
-                        
-                    }
-                }    
-            }
-            // console.log(teachmore)
-            teachjson = {
-                'acceptorId':$('.Wxid').val(),
-                'startDate':class_s.SectBegin,
-                'endDate':class_s.SectEnd,
-                'saveType':'flush',
-                'schoolId':class_s.SchoolId, 
-                'schoolName':class_s.SchoolName,   
-                'classId':class_s.ClassCode,  
-                'className':class_s.ClassName,
-                'teacherId':teachmore.teacherId, 
-                'teacherName':teachmore.teacherName,
-                'masterTeacherId':teachmore.masterTeacherId,
-                'masterTeacherName':teachmore.masterTeacherName,
-                'lessonNo':class_s.LessonNo,
-                'devInfos':stumodata,
-             }
-             // studata.push(teachjson);
-        // }
-        var  teachjsonT = { "Method": "syncStudentsData",'studentsData':teachjson}
-        console.log(teachjsonT)
-        $.ajax({
-                url: 'http://dt.staff.xdf.cn/xdfdtmanager/studentDataController/syncStudentsData.do',
-                type: 'POST',
-                dataType: 'JSON',
-                asyns:false,
-                data:{'studentsData':JSON.stringify(teachjsonT)},
-                success:function(e){    
-                    console.log(e+'1111')
-                    console.log(teachjsonT)
-                    var r = JSON.parse(e);
-                    if(r.code=='200'){
-                        alert('成功')
-                    }
-                }
-            })
-    }
-})
-
-}
-
-
-
-
-// 获取信息
-// var jsona ={"Method":"syncStudentsData","studentsData":{"acceptorId":"0035005A000C514D413535121","startDate":"2017-03-28 17:39:30","endDate":"2017-03-28 17:39:30","saveType":"new","schoolId":"002","schoolName":"机构校区002","classId":"001","className":"演示版本教室一","teacherId":"002","teacherName":"演示版本辅导老师","masterTeacherId":"aode@xdf.cn","masterTeacherName":" 阿城市市场","lessonNo": "1","devInfos": [{"studentName":"student A21","studentId":"A21","studentUserId":"UserId"}]}}
-// var jsonb = JSON.stringify(jsona)
-// var  teachjson = { "Method": "syncStudentsData",'studentsData':studata}
-// $.ajax({
-//     url: 'http://10.200.80.235:8080/xdfdtmanager/studentDataController/syncStudentsData.do',
-//     type: 'POST',
-//     dataType: 'JSON',
-//     asyns:false,
-//     data:{'studentsData':JSON.stringify(teachjson)},
-//     success:function(e){    
-//         console.log(e+'1111')
-//         var r = JSON.parse(e)
-//         if(r.code=='200'){
-//             alert('成功')
-//         }
-//     }
-// })
-//     console.log(teachjson)
-})
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+		<meta name="format-detection" content="telephone=no" />
+		<meta name="full-screen" content="yes"/>
+		<title>扫码</title>
+		<link rel="stylesheet" href="css/Scan.css">
+		<link rel="stylesheet" href="../common/css/base.css"  />
+	</head>
+	<body>
+		<div class="Scan-s sucs"  style="display:none">
+			<img src="images/sys.png" alt=""  id="scanQRCode">
+			<i>请点击图片进行扫一扫</i>
+		</div>
+		<div class="Scan-s erro" style="display:none">
+			<img src="images/ere.png" alt="">
+			<i>登录失败,请重新登录!</i>
+		</div>
+		<input type="text" style="display:none;" class="Wxid">
+		<script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
+		<script src="../common/js/zepto.min.js"></script>
+		<script src="../common/js/rem.js"></script>
+		<script src="js/scanCode.js"></script>
+	</body>
+</html>
