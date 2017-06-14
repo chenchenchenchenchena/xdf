c735e90749b7e276dd150a44a81eb7651e3341b0
v_高文生 <v_gaowensheng@xdf.cn>
2017-06-14 11:29:43 +0800

    Replace login.js

--- src/main/webapp/dtclassroom/js/login.js
+++ src/main/webapp/dtclassroom/js/login.js
@@ -1,32 +1,15 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
-		<meta name="apple-mobile-web-app-capable" content="yes" />
-		<meta name="apple-mobile-web-app-status-bar-style" content="black" />
-		<meta name="format-detection" content="telephone=no" />
-		<meta name="full-screen" content="yes"/>
-		<title>登陆</title>
-		<link rel="stylesheet" href="../common/css/base.css"  />
-		<link rel="stylesheet" href="css/Scan.css"  />
-		<script src="../common/js/zepto.min.js"></script>
-		<script src="../common/js/rem.js"></script>
-		<script src="js/login.js"></script>
-	</head>
-	<body>
-		<section>
-			<!--<form action="" method="post">-->
-				<p class="loginTitle">企业邮箱登陆</p>
-				<p class="email">
-					<input type="text" placeholder="输入企业邮箱" class="email"/>
-					<span>@xdf.cn</span>
-				</p>
-				<input type="password" placeholder="输入邮箱密码" class="pass"/>
-				<!--<input type="submit" value="登陆" class="login"/>-->
-				<button class="login">登陆</button>
-			<!--</form>-->
-			
-		</section>
-	</body>
-</html>
+$(function(){
+	var  cbconfig = {'callbackFlag':'scanCode'};
+	$.ajax({
+	    url: 'http://dt.staff.xdf.cn/xdfdtmanager/e2Login/login.do',
+	    type: 'post',
+	    dataType: 'json',
+	    data:JSON.stringify(cbconfig),
+	    success:function(e){
+	        console.log(e)
+	        location.href = e.url
+	        // window.open('self')
+	        localStorage.e2state = e.e2state
+	    }
+	})
+})
