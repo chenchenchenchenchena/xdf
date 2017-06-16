03bced99a151ba11c53332571bea2354e6a0c0fa
xuyuanhua <xuyuanhua@xdf.cn>
2017-06-16 18:40:49 +0800

    xdfhome代码提交

---
+++ src/main/webapp/homework/js/homeworklist_s.js
@@ -0,0 +1,32 @@
+$(function(){
+	//点击待交作业
+	$(".secul").hide();
+	$(".hwContent").show();
+	$(".hwFinish").hide();
+	$(".hwHeader ul li").click(function(){
+		$(this).addClass("hwShow").siblings("li").removeClass("hwShow");
+		if($(this).index()==0){
+			$(".hwContent").show();
+			$(".hwFinish").hide();
+		}else{
+			$(".hwContent").hide();
+			$(".hwFinish").show();
+		}
+	})
+	//点击已交作业列表
+	var flag=true;
+	$(".firstList").click(function(){
+		if(flag){
+			$(this).css("background","url(images/jiao11.png) no-repeat right 55px");
+			flag=false;
+		}else{
+			$(this).css("background","url(images/jiao22222.png) no-repeat right 55px");
+			flag=true;
+		}
+		$(this).find(".secul").toggle();
+	})
+	//点击作业排行榜
+	$(".hwRankTitle").click(function(){
+		location.href="studentrank.html";
+	})
+})
