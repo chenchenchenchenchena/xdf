d98ff0e50f6bb0203bdef8656bf2c2af1933db31
高文生 <v_gaowensheng@xdf.cn>
2017-06-20 11:24:29 +0800

    提交

--- src/main/webapp/schedule/js/login_t.js
+++ src/main/webapp/schedule/js/login_t.js
@@ -13,38 +13,22 @@ var calbac = {
 
 if(!localStorage.terEmail){
     ajax_S(url.t_more,calbac,teac);
-    var bindingtea0 = {};
-    bindingtea0['email'] = localStorage.terEmail;
-    bindingtea0['wechatId'] = sessionStorage.openid;
-    ajax_S(url.t_wxmo, bindingtea0,binding)//ajax请求
+
 }else{
     etlogin('teacherWX')
 }
 function teac(e){
 	console.log(e);
-	// var i = jQuery.parseJSON(e.data)
-    // $('.name_s').html(i.teacherName);
-    // $('.name_ema').html(i.teacherEmail);
+	// var i = jQuery.parseJSON(e.data);
+    $('.name_s').html(e.data.teacherName);
+    $('.name_ema').html(e.data.teacherEmail);
+    localStorage.terEmail = e.data.teacherEmail
+    var bindingtea0 = {};
+    bindingtea0['email'] = localStorage.terEmail;
+    bindingtea0['wechatId'] = sessionStorage.openid;
+    ajax_S(url.t_wxmo, bindingtea0,binding)//ajax请求
 }
-function teamore(e){
-	console.log(e);
-	if(e.result==true){
-        $('.name_s').html(e.userName);
-        $('.name_ema').html(e.email);
-        localStorage.terEmail = e.email;
-        //var openid = sessionStorage.openid;
-        //if (openid == undefined || openid == '' || openid == 'undefined') {
-        //alert(location.href.substring(0, location.href.indexOf('?code')))
-        //wechatCode(location.href.substring(0,location.href.indexOf('?code')));
-        //}
-        var bindingtea0 = {};
-        bindingtea0['email'] = e.email;
-        bindingtea0['wechatId'] = sessionStorage.openid;
-        ajax_S(url.t_wxmo, bindingtea0,binding)//ajax请求
-	}else{
-		layer.msg(e.message)
-    }
-}
+// s
 
 
 
