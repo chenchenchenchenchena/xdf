bba2728e58f160cd3bd6a762980b38880b9846ca
高文生 <v_gaowensheng@xdf.cn>
2017-06-26 15:32:59 +0800

    提交

--- src/main/webapp/schedule/js/login_t.js
+++ src/main/webapp/schedule/js/login_t.js
@@ -66,6 +66,7 @@ function teac(e){
 function binding(e){
     var a = encodeURI(encodeURI(sessionStorage.nickname))
     $('body').append('<h1>'+decodeURI(decodeURI(a))+'</h1>')
+    $('body').append('<h1>'+sessionStorage.nickname+'</h1>')
 	if(e.result==false){
         ajax_S(url.t_wxmo,WXnum,Wxtea)
 	}else{
