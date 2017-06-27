e269d782d4a5d1f7558ac67f66881f79e9660757
高文生 <v_gaowensheng@xdf.cn>
2017-06-27 19:08:14 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/webapp/schedule/js/login_s.js
+++ src/main/webapp/schedule/js/login_s.js
@@ -119,8 +119,7 @@ function stuc(e){
 		}
 	}
 	function s_bind(e){
-        alert(JSON.stringify(stumore));
-        console.log(e);
+		console.log(e);
 		if(e.data==undefined){
 			layer.msg(e.message);
 			$('.deterAss').html('立即关联');
@@ -139,12 +138,12 @@ function stuc(e){
 
 	//学员号查询点击
 	$('.numb_log').click(function(){
-		var stumore  = {'StudentCode':$('.stunum').val(),'wechatId':sessionStorage.openid,'nickName':encodeURI(sessionStorage.nickname),'headImg': sessionStorage.headimgurl}
+		var stumore  = {'StudentCode':$('.stunum').val(),'wechatId':sessionStorage.openid,'nickName':encodeURIComponent(sessionStorage.nickname),'headImg': sessionStorage.headimgurl}
 		ajax_S(url.s_seac,stumore,stusea)
 	})
 	//关联点击
 	$('.deterAss').click(function(){
-		var stumore  = {'StudentCode':$('.stunum').val(),'wechatId':sessionStorage.openid,'nickName':encodeURI(sessionStorage.nickname),'headImg': sessionStorage.headimgurl}
+		var stumore  = {'StudentCode':$('.stunum').val(),'wechatId':sessionStorage.openid,'nickName':encodeURIComponent(sessionStorage.nickname),'headImg': sessionStorage.headimgurl}
 		if($('.stunum').val()==''){
 			stumore.StudentCode = sessionStorage.stuNumber
 		}
@@ -203,7 +202,7 @@ function stuc(e){
 
 
 $('.tel_log').click(function(){
-	var stuname = {'name':$('.stname').val(),'mobile':$('.phoneNumber').val(),'wechatId':sessionStorage.openid,'nickName':encodeURI(sessionStorage.nickname),'headImg': sessionStorage.headimgurl}
+	var stuname = {'name':$('.stname').val(),'mobile':$('.phoneNumber').val(),'wechatId':sessionStorage.openid,'nickName':encodeURIComponent(sessionStorage.nickname),'headImg': sessionStorage.headimgurl}
 	// var stuname = {};
 	// var name = $('.stname').val();
 	// var mobile  = $('.phoneNumber').val();
@@ -237,12 +236,12 @@ function telbind(e){
 $(document).on('click','.Relation',function(){
 	if($(this).html()=='确认关联'){
 		var stunum  = $('.stu_num').eq($(this).parent().index()-1).text()
-		var stumore = {'StudentCode':stunum,'wechatId':sessionStorage.openid,'Mobile':sessionStorage.stuTel,'nickName':encodeURI(sessionStorage.nickname),'headImg': sessionStorage.headimgurl}
+		var stumore = {'StudentCode':stunum,'wechatId':sessionStorage.openid,'Mobile':sessionStorage.stuTel,'nickName':encodeURIComponent(sessionStorage.nickname),'headImg': sessionStorage.headimgurl}
 		ajax_S(url.s_bind,stumore,telbind)
 		// $(this).html('解除关联')
 	}else{
 		var stunum  = $('.stu_num').eq($(this).parent().index()-1).text()
-        var stumore = {'StudentCode':stunum,'wechatId':sessionStorage.openid,'nickName':encodeURI(sessionStorage.nickname),'headImg': sessionStorage.headimgurl}
+        var stumore = {'StudentCode':stunum,'wechatId':sessionStorage.openid,'nickName':encodeURIComponent(sessionStorage.nickname),'headImg': sessionStorage.headimgurl}
 		ajax_S(url.s_nobd,stumore,telbind)
 		$(this).html('确认关联')
     }
