183781458ebadcd2dad5e220be5d847cc0961e62
高文生 <v_gaowensheng@xdf.cn>
2017-06-28 14:08:51 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/webapp/schedule/js/schedule_s.js
+++ src/main/webapp/schedule/js/schedule_s.js
@@ -88,6 +88,7 @@ $(function(){
             location.href = 'login_s.html';
             sessionStorage.callbackconfig = 'schedule'
         }else if(localStorage.terEmail){
+
             location.href = 'schedule_t.html';
             sessionStorage.removeItem('callbackconfig')
         }else{
