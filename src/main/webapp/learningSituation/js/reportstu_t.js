fc30571956f382fbc70828c9189ade9c1ecd44f6
高文生 <v_gaowensheng@xdf.cn>
2017-06-30 18:00:36 +0800

    提交

--- src/main/webapp/learningSituation/js/reportstu_t.js
+++ src/main/webapp/learningSituation/js/reportstu_t.js
@@ -2,43 +2,47 @@ $(function(){
 
 
 
+var maxindex = '';
+
+    /*
+     Xtwindex   //x轴
+     Cindex     //出门测
+     Rindex     //入门测
+     */
 var Xtwindex = [];
-
-
+var Cindex = [];
+var Rindex = [];
 
 
 var Xindex = '';
+
+
+
+
+//切换
+
+
+
+
+
+
+
+
+
 ajaxRequest('post','http://dt.staff.xdf.cn/xdfdtmanager/teacherAnalysis/scoreStdReport.do',{'teacherEmail':'caoxuefeng@xdf.cn','classCode':'CZSPP008','tCode':'1','studentNo':'SS2431'},		function(e){
-            if(sessionStorage.tCode==1){
-
-            }else{
-                console.log(e)
-                Xindex = e.data
+            console.log(e);
+            maxindex = e.data[e.data.length-1].lessonNo
+            for(var i = 0;i<e.data.length;i++) {
+                Rindex.push(e.data[i].AvgGrade);
+                $('.reportstu_S ul').eq(0).append('<li>'+(i+1)+'</li>')
+                $('.reportstu_S ul').eq(1).append('<li>'+e.data[i].realGrade+'</li>')
+                $('.reportstu_S ul').eq(2).append('<li>'+e.data[i].AvgGrade+'</li>')
             }
-            /*
-             Xtwindex   //x轴
-             Cindex     //出门测
-             Rindex     //入门测
-             */
-
-
-    $('.title_s').eq(0).siblings().show();
-    $('.title_s').eq(0).find('img').css('transform','rotate(-90deg)')
-
-
-
-
-
-});
-
-    Echart('chart_S',[1,2,3,4],[7,9,8,10],[,9.4,8.4,   6.8])
-
-
-
-
-function Echart(id,x,y1,y2){
-var myChart = echarts.init(document.getElementById(id));
-var option = {
+            for(var j = 0;j<maxindex;j++){
+                Xtwindex.push(j)
+            }
+    var myChart = echarts.init(document.getElementById('chart_S'));
+    var option = {
         tooltip : {
             trigger: 'axis',
             triggerOn:'click',
@@ -56,7 +60,7 @@ var option = {
                 name:'课次',
                 type : 'category',
                 boundaryGap : false,
-                data :x,
+                data :Xtwindex,
                 nameTextStyle:{
                     fontSize:24
                 },
@@ -88,21 +92,7 @@ var option = {
             {
                 name:'出门测',
                 type:'line',
-                data:y1,
-                nameTextStyle:{
-                    fontSize:24
-                },
-                axisLabel: {
-                    show: true,
-                    textStyle: {
-                        fontSize: 24
-                    }
-                }
-            },
-            {
-                name:'入门测',
-                type:'line',
-                data:y2,
+                data:Rindex,
                 nameTextStyle:{
                     fontSize:24
                 },
@@ -129,6 +119,28 @@ var option = {
 
     // 为echarts对象加载数据
     myChart.setOption(option);
+            /*
+             Xtwindex   //x轴
+             Cindex     //出门测
+             Rindex     //入门测
+             */
+
+
+
+
+
+
+
+
+});
+
+    $('.title_s').eq(0).siblings().show();
+    $('.title_s').eq(0).find('img').css('transform','rotate(-90deg)')
+
+
+
+function Echart(id,x,y1,y2){
+
 
 }
 
