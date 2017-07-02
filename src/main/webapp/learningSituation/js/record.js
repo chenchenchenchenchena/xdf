5301a3050209b96237730aa16c4b791ca306ae73
v_zhaoshuang2 <v_zhaoshuang2@xdf.cn>
2017-07-02 18:21:49 +0800

    Signed-off-by: v_zhaoshuang2 <v_zhaoshuang2@xdf.cn>

--- src/main/webapp/learningSituation/js/record.js
+++ src/main/webapp/learningSituation/js/record.js
@@ -388,6 +388,7 @@ $(function () {
 			    	"schoolId":sessionStorage.schoolId,
 			    	"lessonNo":$(".classTime").html().substring(1,2)
 			    };
+			    alert(queryData.teacherEmail)
 			    console.log(queryData.lessonNo)
 			     console.log(queryData.classCode)
 			     if ($(".st").html() == "入门测") {
