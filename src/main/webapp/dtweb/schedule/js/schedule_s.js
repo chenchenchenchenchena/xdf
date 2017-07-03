542eb4f4c8ec22f7d0f26156b264f43a9e21913d
v_liwei8 <v_liwei8@xdf.cn>
2017-07-03 17:50:19 +0800

    dtweb:修改课程界面

--- src/main/webapp/dtweb/schedule/js/schedule_s.js
+++ src/main/webapp/dtweb/schedule/js/schedule_s.js
@@ -4,7 +4,7 @@ $(function(){
     //    wechatCode(location.href)
     //    return false;
     //}
-    wechatCode(location.href);
+    // wechatCode(location.href);
     //判断ios
     var u = navigator.userAgent;
     var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
@@ -48,52 +48,52 @@ $(function(){
         'endDate':new Date().format("yyyy-MM")+'-'+getCountDays()
     };
     // 微信查询是否绑定微信  参数：当前微信号 学生
-    ajax_S(url.s_seac,WXnum,stud);
-    $('.js_jin').click(function(){
-        var emailm = {
-            'studentCode':sessionStorage.stuNum,
-            'beginDate':time1,
-            'endDate':time1
-        };
-        ajax_S(url.s_stud,emailm,stusea);
-        var month  = $('.today').attr('data_m');
-        var  day = new Date($('#ymym').html().substring(0,4),month,'0');
-        var daycount = day.getDate();
-        var menu_s = {
-            'studentCode':sessionStorage.stuNum,
-            'beginDate':$('#ymym').html().substring(0,4)+'-'+month+'-01',
-            'endDate':$('#ymym').html().substring(0,4)+'-'+month+'-'+daycount
-        };
-        ajax_S(url.s_stud,menu_s,menufunc);
-        monththis = month
-    })
+    // ajax_S(url.s_seac,WXnum,stud);
+    // $('.js_jin').click(function(){
+    //     var emailm = {
+    //         'studentCode':sessionStorage.stuNum,
+    //         'beginDate':time1,
+    //         'endDate':time1
+    //     };
+    //     ajax_S(url.s_stud,emailm,stusea);
+    //     var month  = $('.today').attr('data_m');
+    //     var  day = new Date($('#ymym').html().substring(0,4),month,'0');
+    //     var daycount = day.getDate();
+    //     var menu_s = {
+    //         'studentCode':sessionStorage.stuNum,
+    //         'beginDate':$('#ymym').html().substring(0,4)+'-'+month+'-01',
+    //         'endDate':$('#ymym').html().substring(0,4)+'-'+month+'-'+daycount
+    //     };
+    //     ajax_S(url.s_stud,menu_s,menufunc);
+    //     monththis = month
+    // })
     // 微信查询是否绑定微信  参数：当前微信号 学生
-    function stud(e){
-        if(e.result==false){
-            // 微信查询是否绑定微信  参数：当前微信号 老师
-            ajax_S(url.t_wxmo,WXnum,teac);
-        }else{
-            //存储学员号
-            sessionStorage.stuNum = e.data.studentNo;
-            emailm.studentCode=sessionStorage.stuNum;
-            menu_s.studentCode=sessionStorage.stuNum;
-            ajax_S(url.s_stud,menu_s,menufunc);
-            ajax_S(url.s_stud,emailm,stusea);
-        }
-    }
+    // function stud(e){
+    //     if(e.result==false){
+    //         // 微信查询是否绑定微信  参数：当前微信号 老师
+    //         ajax_S(url.t_wxmo,WXnum,teac);
+    //     }else{
+    //         //存储学员号
+    //         sessionStorage.stuNum = e.data.studentNo;
+    //         emailm.studentCode=sessionStorage.stuNum;
+    //         menu_s.studentCode=sessionStorage.stuNum;
+    //         ajax_S(url.s_stud,menu_s,menufunc);
+    //         ajax_S(url.s_stud,emailm,stusea);
+    //     }
+    // }
 
     // 微信查询是否绑定微信  参数：当前微信号 老师
-    function teac(e){
-        if(e.data=="goE2"){
-            location.href = 'login_s.html';
-            sessionStorage.callbackconfig = 'schedule'
-        }else if(localStorage.terEmail){
-            location.href = 'schedule_t.html';
-            sessionStorage.removeItem('callbackconfig')
-        }else{
-            location.href = 'login_s.html';
-        }
-    }
+    // function teac(e){
+    //     if(e.data=="goE2"){
+    //         location.href = 'login_s.html';
+    //         sessionStorage.callbackconfig = 'schedule'
+    //     }else if(localStorage.terEmail){
+    //         location.href = 'schedule_t.html';
+    //         sessionStorage.removeItem('callbackconfig')
+    //     }else{
+    //         location.href = 'login_s.html';
+    //     }
+    // }
     //学生查询课程  整月查询
     function menufunc(e){
         var arr = [];
