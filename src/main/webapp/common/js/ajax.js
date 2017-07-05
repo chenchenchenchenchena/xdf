5f0e215507180e6a5a881579aa8adf398e9f0c12
高文生 <v_gaowensheng@xdf.cn>
2017-07-05 21:36:46 +0800

    提交

--- src/main/webapp/common/js/ajax.js
+++ src/main/webapp/common/js/ajax.js
@@ -4,12 +4,24 @@ var url_o = 'http://dt.xdf.cn/xdfdtmanager/';
 var url_o2 = 'http://dt.xdf.cn';
 var appId = '';
 var secreT = '';
+
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-20005525-5', 'auto');
 ga('send', 'pageview');
+
+
+
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?55f7f2243b727e03faa4a3be905928f3";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
 
 if (window.location.host == onlineUrl) {//正式环境
     url_o = 'http://dt.xdf.cn/xdfdtmanager/';
