737ed592644c4b4896df93fe02640e8d9eff0de5
v_zhaoshuang2 <v_zhaoshuang2@xdf.cn>
2017-07-07 10:46:37 +0800

    Signed-off-by: v_zhaoshuang2 <v_zhaoshuang2@xdf.cn>

--- src/main/webapp/dtweb/learningSituation/js/record.js
+++ src/main/webapp/dtweb/learningSituation/js/record.js
@@ -216,7 +216,7 @@ $(function () {
    
     function queryOpenid(){
     	alert("1111");
-    	 var queryOpenid={
+    	var queryOpenid={
     		"schoolId":localStorage.schoolId,
     		"stuQuery":stuQuery
     	}
@@ -339,7 +339,7 @@ $(function () {
     
     $(".recordSucc button").click(function(){
      	layer.close(layer3);
-     	location.href="report_t.html";
+     	/*location.href="report_t.html";*/
      })
     $(".noRecord button").eq(0).click(function(){
      	layer.close(layer1);
