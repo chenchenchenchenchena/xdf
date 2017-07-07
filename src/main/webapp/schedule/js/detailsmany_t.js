6c5456736851eefa120620a9d190e2187ed2e72b
高文生 <v_gaowensheng@xdf.cn>
2017-07-07 17:31:53 +0800

    提交

--- src/main/webapp/schedule/js/detailsmany_t.js
+++ src/main/webapp/schedule/js/detailsmany_t.js
@@ -25,7 +25,7 @@ $(function(){
 			}
 		}
 		var masterta = e.data.Data[timeindex].Teachers.split(',');
-		var subject = e.data.Data[timeindex].CourseCode;
+		var subject = e.data.Data[timeindex].ClassCode;
 		for(var v = 0;v<e.subject.length;v++){
 			if(e.subject[v].courseName==subject){
                 subject_s = e.subject[v].subject
