ed45bf158818a518826eb7e2ab0286682b5f323f
hulihua <zhangsjup@163.com>
2017-07-11 18:17:29 +0800

    图片预览

--- src/main/webapp/homework/js/dohomework.js
+++ src/main/webapp/homework/js/dohomework.js
@@ -289,9 +289,15 @@ $(function () {
     // 图片预览
     $(document).on('touchend', '.imgBox>div>img', function (){
         alert("预览图片"+$(this).attr('src'));
+        var previewUrl = "";
+        if($(this).attr('src').indexOf('weixin://')!= -1){
+            previewUrl = $(this).attr('src');
+        }else{
+            previewUrl = 'http://dt.staff.xdf.cn/xdfdthome/homework/'+$(this).attr('src');
+        }
         wx.previewImage({
-            current: 'http://dt.staff.xdf.cn/xdfdthome/homework/'+$(this).attr('src'), // 当前显示图片的http链接
-            urls: ['http://dt.staff.xdf.cn/xdfdthome/homework/'+$(this).attr('src')] // 需要预览的图片http链接列表
+            current: previewUrl, // 当前显示图片的http链接
+            urls: [previewUrl] // 需要预览的图片http链接列表
         });
     });
 
