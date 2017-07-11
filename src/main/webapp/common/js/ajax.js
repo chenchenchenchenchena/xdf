a3fffa126d37ad4b1bfa2d9081d420df86041ba7
v_zhaoshuang2 <v_zhaoshuang2@xdf.cn>
2017-07-11 11:15:35 +0800

    Signed-off-by: v_zhaoshuang2 <v_zhaoshuang2@xdf.cn>

--- src/main/webapp/common/js/ajax.js
+++ src/main/webapp/common/js/ajax.js
@@ -72,7 +72,8 @@ var url = {
     't_webback':url_o2+'/xdfdthome/dtweb/schedule/login_s.html', //回调地址
     't_webmore':url_o+'teacherBind/pcqueryTeacherInfo.do', //查询教师信息
     'w_token':url_o+'/wechatSignature/getWechatToken.do',
-    'w_openId':url_o+'teacherAnalysis/queryStudentWechat.do'
+    'w_openId':url_o+'teacherAnalysis/queryStudentWechat.do',
+    'w_push':url_o+'wechatSignature/sendTemplateMsg.do'//微信推送成绩信息
 };
 var Study = {
     's_study':url_o+'studentAnalysis/scoreStdIdlReportStatus.do',  //学生获取个人成绩
