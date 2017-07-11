d85b465e0cc9776bca92f823256294de480f07ba
高文生 <v_gaowensheng@xdf.cn>
2017-07-11 19:23:13 +0800

    提交

--- src/main/webapp/homework/js/homeworklist_t.js
+++ src/main/webapp/homework/js/homeworklist_t.js
@@ -4,7 +4,9 @@ $(function(){
     //tab
 	$(document).on('touchstart','.hwFinish .firstList',function(){
 	    if($(this).find('ul').css('display')=='none'){
-	        
+            ajaxRequest('post',homework_s.t_stat,{teacherEmail:'caoxuefeng@xdf.cn',classCode:$(this).attr('classCode'),schoolId:'73',courseCode:$(this).attr('courseCode')},function(e){
+            });
+            $(this).find('.state_st').hide();
 	       $(this).find('ul').show();
            $(this).css("background","url(images/jiao11.png) no-repeat right 55px");
         }else{
@@ -45,6 +47,7 @@ $(function(){
     });
 
 
+    var Read = '';
 	// 获取老师作业列表
     ajax_S(homework_s.t_list,{email:'caoxuefeng@xdf.cn','schoolId':'73'},function(e){
         console.log(e);
@@ -56,18 +59,24 @@ $(function(){
         var list_s = e.data;
         $('.Prompt_s i').html(e.SumUpnotCorrect);
         for(var a = 0;a<list_s.length;a++){
-            $('.hwFinish>ul').append('<li class="firstList"> <p style="display:inline;">'+list_s[a][0].className+'&nbsp;('+list_s[a][0].studentNum+'人)</p><span class="state_st"></span><ul class="secul tealist_s"></ul></li>')
+            list_s[a][0].readStatus==0?Read='state_st':Read='';
+            $('.hwFinish>ul').append('<li class="firstList" classCode="'+list_s[a][0].classCode+'" courseCode="'+list_s[a][0].courseCode+'"> <p style="display:inline;">'+list_s[a][0].className+'&nbsp;('+list_s[a][0].studentNum+'人)</p><span class='+Read+'></span><ul class="secul tealist_s"></ul></li>')
             for(var b = 0;b<list_s[a].length;b++){
-                $('.tealist_s').eq(a).append(' <li><span>'+list_s[a][b].lessonTime.split(' ')[0].substr(5,6)+'</span><p class="state_s">已批:'+list_s[a][b].yescommit+'/未批:'+list_s[a][b].nocorrect+'/未交:'+list_s[a][b].yescorrect+'</p><span class="more_so">查看 <img src="images/B02-2_03.png" alt="" /></span><div class="remove_s"><span>修改</span><span class="delete_s">删除</span></div></li> ')
+                $('.tealist_s').eq(a).append(' <li><span>'+list_s[a][b].homeworkTime+'</span><p class="state_s">已批:'+list_s[a][b].yescommit+'/未批:'+list_s[a][b].nocorrect+'/未交:'+list_s[a][b].yescorrect+'</p><span class="more_so"  classCode="'+list_s[a][0].classCode+'" courseCode="'+list_s[a][0].courseCode+'" homeworkTime="'+list_s[a][b].homeworkTime+'">查看 <img src="images/B02-2_03.png" alt="" /></span><div class="remove_s"><span>修改</span><span class="delete_s">删除</span></div></li> ')
             }
-
         }
-    })
+    });
+    //删除
+    $(document).on('touchend','.delete_s',function(){
 
-
-
+    });
     //查看更多
-
+    $(document).on('touchend','.more_so',function(){
+        sessionStorage.classCode_s = $(this).attr('classCode');
+        sessionStorage.courseCode_s = $(this).attr('courseCode');
+        sessionStorage.homeworkTime_s = $(this).attr('homeworkTime');
+        location.href = 'reply_t.html';
+    });
 
 
 
