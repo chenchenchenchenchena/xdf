57a2ef71bd66c9a4ffc6e620bf0dd4fd05a66021
v_liwei8 <v_liwei8@xdf.cn>
2017-07-12 18:08:05 +0800

    图片上传接口

--- src/main/webapp/homework/js/dohomework.js
+++ src/main/webapp/homework/js/dohomework.js
@@ -95,8 +95,8 @@ $(function () {
             'classId': "hx001"
         };
         $.ajax({
-            url: url_o + "upload/uploadAudio.do",
-            // url: "http://10.200.80.235:8080/xdfdtmanager/upload/uploadAudio.do",
+            // url: url_o + "upload/uploadAudio.do",
+            url: "http://10.200.80.235:8080/xdfdtmanager/upload/uploadAudio.do",
             type: 'post',
             dataType: 'json',
             data: cbconfig,
