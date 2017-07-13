2b854a8b8fcefc8b5f8423392ca112b9439c51bc
v_liwei8 <v_liwei8@xdf.cn>
2017-07-13 15:49:13 +0800

    获取文件

--- src/main/webapp/homework/js/finishedhomework.js
+++ src/main/webapp/homework/js/finishedhomework.js
@@ -295,7 +295,7 @@ $(function () {
             dataType: 'json',
             data: optionFile,
             success: function (e) {
-                alert(JSON.stringify(e));
+                // alert(JSON.stringify(e));
                 if (e.success == false) {
                     alert(e.message);
                 } else {
