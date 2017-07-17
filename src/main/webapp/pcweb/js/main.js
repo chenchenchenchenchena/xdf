9b9e38867e332041de0d371fe8006604630069e0
v_liwei8 <v_liwei8@xdf.cn>
2017-07-17 18:36:28 +0800

    修改参数

--- src/main/webapp/pcweb/js/main.js
+++ src/main/webapp/pcweb/js/main.js
@@ -47,7 +47,8 @@ function showFunctionList(json) {
 
         if(functionList == undefined){
             alert("用户名不存在");
-            // window.top.location.href = getRootPath();
+            clearCookie();
+            window.top.location.href = getRootPath();
         }else{
             //获取functionIds
             setFunctionList(json.functionList);
