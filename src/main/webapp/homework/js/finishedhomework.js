2958b4bf46317eafc5afc4012d2f23796ce08227
hulihua <zhangsjup@163.com>
2017-07-17 19:18:18 +0800

    阅读状态处理

--- src/main/webapp/homework/js/finishedhomework.js
+++ src/main/webapp/homework/js/finishedhomework.js
@@ -85,7 +85,7 @@ $(function () {
             success: function (e) {
                 // alert(JSON.stringify(e));
                 if (e.success == false) {
-                    alert(e.message);
+                    console.log(e.message);
                 } else {
                     //将文件显示到布局中
                     if (fileType.indexOf("mp3") != -1) {
@@ -144,13 +144,13 @@ $(function () {
     }
     /*--------------------图片预览----------------------------------*/
     $(document).on('touchend', '.imgBox img', function () {
-        alert("预览图片" + $(this).attr('src'));
+        // alert("预览图片" + $(this).attr('src'));
         var previewUrl = $(this).attr('src');
-        if ($(this).attr('src').indexOf('weixin://') == -1&&$(this).attr('src').indexOf('http:') == -1) {
-            previewUrl = 'http://dt.staff.xdf.cn/xdfdthome/homework/' + $(this).attr('src');
-        } else {
-            previewUrl = $(this).attr('src');
-        }
+        // if ($(this).attr('src').indexOf('weixin://') == -1&&$(this).attr('src').indexOf('http:') == -1) {
+        //     previewUrl = 'http://dt.staff.xdf.cn/xdfdthome/homework/' + $(this).attr('src');
+        // } else {
+        //     previewUrl = $(this).attr('src');
+        // }
         wx.previewImage({
             current: previewUrl, // 当前显示图片的http链接
             urls: [previewUrl] // 需要预览的图片http链接列表
