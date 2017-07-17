9b5d4ac36319050253c165e0e76d115856372aca
高文生 <v_gaowensheng@xdf.cn>
2017-07-17 19:32:57 +0800

    提交

--- src/main/webapp/homework/js/Assignment.js
+++ src/main/webapp/homework/js/Assignment.js
@@ -36,7 +36,7 @@ $(function () {
                 var tea = e.data;
                 for(var b  = 0;b<tea.length;b++){
                     if(tea[b].fileType=='mp3'){
-                        $('.big_s').eq(0).append('<div class="music_s" fileName="'+tea[b].fileName+'" fileType="'+tea[b].fileType+'" fileSize="'+tea[b].fileSize+'" diskFilePath="'+tea[b].diskFilePath+'"><span>10"</span> <audio  src="'+tea[b].previewUrl+'" id="bgMusic'+b+'"></audio ></div>')
+                        $('.big_s').eq(0).append('<div class="music_s" fileName="'+tea[b].fileName+'" fileType="'+tea[b].fileType+'" fileSize="'+tea[b].fileSize+'" diskFilePath="'+tea[b].diskFilePath+'"><span>10"</span> <audio  src="'+tea[b].previewUrl+'" id="bgMusic'+b+'" onclick="PlaySound(this)"></audio ></div>')
                     }else{
                         $('.imgBox').show();
                         $('.imgBox').eq(0).append('<img src="'+tea[b].thumbnail+'" alt="" fileName="'+tea[b].fileName+'" fileType="'+tea[b].fileType+'" fileSize="'+tea[b].fileSize+'" diskFilePath="'+tea[b].diskFilePath+'"/>')
@@ -453,7 +453,13 @@ $(function () {
         $('.music_s').eq(Index_s).find('span').html(timeInedex + '"');
     }
 
+    function PlaySound(soundobj) {
 
+        var thissound = document.getElementById(soundobj);
+        alert(5);
+        thissound.play();
+
+    }
     $(document).on('touchend','.music_s',function () {
         if($(this).find('audio')){
             alert(0);
