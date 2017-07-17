587ba5b8036d622ee23e715dfe6cfd9ef69ca9c3
高文生 <v_gaowensheng@xdf.cn>
2017-07-17 18:41:03 +0800

    提交

--- src/main/webapp/homework/js/Assignment.js
+++ src/main/webapp/homework/js/Assignment.js
@@ -36,15 +36,13 @@ $(function () {
                 var tea = e.data;
                 for(var b  = 0;b<tea.length;b++){
                     if(tea[b].fileType=='mp3'){
-                        $('.big_s').eq(0).append('<div class="music_s" fileName="'+tea[b].fileName+'" fileType="'+tea[b].fileType+'" fileSize="'+tea[b].fileSize+'" diskFilePath="'+tea[b].diskFilePath+'"><span>10"</span> <video  src="'+tea[b].previewUrl+'" id="bgMusic"></video ></div>')
+                        $('.big_s').eq(0).append('<div class="music_s" fileName="'+tea[b].fileName+'" fileType="'+tea[b].fileType+'" fileSize="'+tea[b].fileSize+'" diskFilePath="'+tea[b].diskFilePath+'"><span>10"</span> <audio  src="'+tea[b].previewUrl+'" id="bgMusic"></audio ></div>')
                     }else{
                         $('.imgBox').show();
                         $('.imgBox').eq(0).append('<img src="'+tea[b].thumbnail+'" alt="" fileName="'+tea[b].fileName+'" fileType="'+tea[b].fileType+'" fileSize="'+tea[b].fileSize+'" diskFilePath="'+tea[b].diskFilePath+'"/>')
                     }
                 }
             });
-
-
         }
     });
 
@@ -457,6 +455,20 @@ $(function () {
 
 
     $(document).on('touchend','.music_s',function () {
+        if($(this).find('audio')){
+            alert(0);
+            var audio = document.getElementById('audio');
+            if(audio!==null){
+                if(!audio.paused)
+                {
+// 这个就是暂停
+                    audio.pause();
+                }else{
+// 这个就是播放
+                    audio.play();
+                }
+            }
+        }
         $(this).addClass('playing_s');
         playVoice(song_s);
         setTimeout(function(){
