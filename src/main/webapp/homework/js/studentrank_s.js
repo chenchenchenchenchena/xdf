d10473f75098dda7413cec88e8203abbe7cff343
hulihua <zhangsjup@163.com>
2017-07-19 09:14:34 +0800

    排行榜显示时间处理

--- src/main/webapp/homework/js/studentrank_s.js
+++ src/main/webapp/homework/js/studentrank_s.js
@@ -5,83 +5,49 @@ $(function(){
     var loading;
     $(".ranklist,.stuempty").hide();
     var reqData = 'homeworkTinfoId='+localStorage.hwteacherId;
-    // loading = layer.load();
+    loading = layer.load();
     ajaxRequest('GET', homework_s.s_hwrank, reqData, getHwRankSuccess);
-})
-
-//获取学生排行
-function getHwRankSuccess(msg) {
-    var msg1 = {
-        "code": "200",
-        "data": [
-            {
-                "updateTime": "",
-                "lessonTime": "2017-07-11 15:30:00",
-                "stuName": "崔志璞"
-            },
-            {
-                "updateTime": "",
-                "lessonTime": "2017-07-11 15:30:00",
-                "stuName": "崔志璞"
-            },
-            {
-                "updateTime": "",
-                "lessonTime": "2017-07-11 15:30:00",
-                "stuName": "崔志璞"
-            },
-            {
-                "updateTime": "",
-                "lessonTime": "2017-07-11 15:30:00",
-                "stuName": "崔志璞"
-            },
-            {
-                "updateTime": "",
-                "lessonTime": "2017-07-11 15:30:00",
-                "stuName": "崔志璞"
-            },
-            {
-                "updateTime": "",
-                "lessonTime": "2017-07-11 15:30:00",
-                "stuName": "高炬文"
-            }
-        ],
-        "status": "success"
-    };
-    $(".ranklist").html(" ");
-    if(msg.code==200){
-        if(msg.data!='undefined' && msg.data.length>0){
-            var datas = msg.data;
-            $.each(datas,function(i,items){
-                console.log(i);
-                var rankCss,ranking,studentNo;
-                //名次样式
-                switch(i){
-                    case 0:
-                        rankCss = "rankfirst";
-                        break;
-                    case 1:
-                        rankCss = "ranksecond";
-                        break;
-                    case 2:
-                        rankCss = "rankthird";
-                        break;
-                    default :
-                        rankCss = "rankcom";
-                        break;
-                }
-                var rankListHtml='<li><span class="rankleft"><i class="'+rankCss+'">'+parseInt(i+1)+'</i><i>'
-                    +items.stuName.substr(-2,2)+'</i><i>'+items.stuName
-                    +'</i></span><span class="rankright"><i>'+items.homeworkTime+'</i></span></li>';
-                $(".ranklist").append(rankListHtml);
-                $(".ranklist").show();
+    //获取学生排行
+    function getHwRankSuccess(msg) {
+        $(".ranklist").html(" ");
+        if(msg.code==200){
+            if(msg.data!='undefined' && msg.data.length>0){
+                var datas = msg.data;
+                $.each(datas,function(i,items){
+                    console.log(i);
+                    var rankCss,ranking,studentNo;
+                    //名次样式
+                    switch(i){
+                        case 0:
+                            rankCss = "rankfirst";
+                            break;
+                        case 1:
+                            rankCss = "ranksecond";
+                            break;
+                        case 2:
+                            rankCss = "rankthird";
+                            break;
+                        default :
+                            rankCss = "rankcom";
+                            break;
+                    }
+                    var rankListHtml='<li><span class="rankleft"><i class="'+rankCss+'">'+parseInt(i+1)+'</i><i>'
+                        +items.stuName.substr(-2,2)+'</i><i>'+items.stuName
+                        +'</i></span><span class="rankright"><i>'+items.updateTime.substr(0,16)+'</i></span></li>';
+                    $(".ranklist").append(rankListHtml);
+                    $(".ranklist").show();
+                    // layer.close(loading);
+                });
+            }else{
                 // layer.close(loading);
-            });
+                $('.stuempty').show();
+            }
         }else{
-            // layer.close(loading);
+            console.log("err:"+JSON.stringify(msg));
             $('.stuempty').show();
         }
-    }else{
-        console.log("err:"+JSON.stringify(msg));
+        layer.close(loading);
     }
+})
 
-}
+
