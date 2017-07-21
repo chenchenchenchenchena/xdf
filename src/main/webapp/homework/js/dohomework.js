dd7c0c95b9684069e7b991d6746411ad65f5b115
hulihua <zhangsjup@163.com>
2017-07-21 15:14:37 +0800

    修改调用接口

--- src/main/webapp/homework/js/dohomework.js
+++ src/main/webapp/homework/js/dohomework.js
@@ -636,8 +636,7 @@ $(function () {
     $(document).on('touchend', '.submitBox .confirmBtn', function () {
         layer.close(layer2);
         // window.location.href = 'homeworklist_s.html';
-        window.history.go(-1);
-        window.location.reload();
+        window.history.go(-1);location.reload();
     });
     //提交作业--失败--取消
     $(document).on('touchend', '.submitFail .cancelBtn', function () {
