dc54368723aa644283af3b02270408e316f132b5
v_gaowensehng <6-FMX9j&cf>
2017-07-24 15:36:44 +0800

    提交

--- src/main/webapp/homework/js/replydetail_t.js
+++ src/main/webapp/homework/js/replydetail_t.js
@@ -571,6 +571,10 @@ $(function () {
 
     /*--------------------图片预览----------------------------------*/
     $(document).on('touchend', '.imgBox img', function () {
+        $('.esc_s').hide();
+        $('.true_s').hide();
+        $('.big_back_s span:last-of-type').show();
+        $('.big_back_s img').show();
         Index_Last = $(this).parent().index();
         var previewUrl = $(this).attr('src');
         $('.big_back_s canvas').hide();
@@ -594,6 +598,7 @@ $(function () {
     $('.esc_s').on('touchend',function(){
         $('.big_back_s').hide();
     });
+    var Imgurl = '';
     $('.big_back_s span:last-of-type').on('touchend',function(){
         $(this).hide();
         $('.true_s').show();
@@ -613,6 +618,7 @@ $(function () {
         ctx.drawImage(img,0,0,width_,height);
         $('.big_back_s img').hide();
         $('.big_back_s canvas').show();
+        Imgurl  = canvas.toDataURL("image/png");
         // canvas事件
         $(document).on('touchstart','canvas',function(){
             ctx.beginPath();
@@ -628,11 +634,16 @@ $(function () {
             });
 
         });
-        $('.true_s').on('touchend',function(){
-            $('.notsubmit .imgBox').show();
-            $('.notsubmit .imgBox').append("<li><span class='stuImg' img-index='" + Index_Last + "'></span><img src='" +canvas.toDataURL("image/png") + "'/></li>");
-        });
+
         return false;
+    });
+    $('.true_s').on('touchend',function(){
+        $('.notsubmit .imgBox').show();
+        $('.notsubmit .imgBox').append("<li><span class='stuImg' img-index='" + Index_Last + "'></span><img src='" +Imgurl+ "'/></li>");
+        $('.big_back_s').show();
+        setTimeout(function(){
+            $('.big_back_s').hide();
+        },300)
     });
     /*-------------------- 删除语音 --------------------*/
     $(document).on('touchend', '.stuVoice', function () {
