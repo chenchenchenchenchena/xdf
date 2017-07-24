9b9413ed67c86bfdb2648c9fb0f40205a5798f20
v_liwei8 <v_liwei8@xdf.cn>
2017-07-24 09:59:30 +0800

    修改：老师修改作业时图片语音显示以及无法修改问题

--- src/main/webapp/homework/js/Assignment.js
+++ src/main/webapp/homework/js/Assignment.js
@@ -39,7 +39,7 @@ $(function () {
                 var tea = e.data;
                 for (var b = 0; b < tea.length; b++) {
                     if (tea[b].fileType == 'mp3') {
-                        showAudio(url_o +tea[b].diskFilePath, $('#record_audio_box'), recordCount);
+                        showAudio(url_o +tea[b].url, $('#record_audio_box'), recordCount);
                         // $('.big_s').eq(0).append('<div class="music_s" onclick="PlaySound(bgMusic' + b + ')"  fileName="' + tea[b].fileName + '" fileType="' + tea[b].fileType + '" fileSize="' + tea[b].fileSize + '" diskFilePath="' + tea[b].diskFilePath + '"><span>10"</span> <audio  src="' + tea[b].previewUrl + '" id="bgMusic' + b + '"  controls="controls" preload="auto"></audio ></div>')
                     } else {
                         showUpdataImage(b, tea[b].url);
