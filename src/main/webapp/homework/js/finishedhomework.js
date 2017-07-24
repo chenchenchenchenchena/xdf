a9ddf2b1df46723ba81c2064093a5966986db308
hulihua <zhangsjup@163.com>
2017-07-24 11:20:32 +0800

    添加学生修改作业判断

--- src/main/webapp/homework/js/finishedhomework.js
+++ src/main/webapp/homework/js/finishedhomework.js
@@ -215,22 +215,6 @@ $(function () {
         str += "</div>";
         $('#' + imageId).append(str);
     }
-
-    /*--------------------图片预览----------------------------------*/
-    $(document).on('touchend', '.imgBox img', function () {
-        // alert("预览图片" + $(this).attr('src'));
-        var previewUrl = $(this).attr('src');
-        // if ($(this).attr('src').indexOf('weixin://') == -1&&$(this).attr('src').indexOf('http:') == -1) {
-        //     previewUrl = 'http://dt.staff.xdf.cn/xdfdthome/homework/' + $(this).attr('src');
-        // } else {
-        //     previewUrl = $(this).attr('src');
-        // }
-        wx.previewImage({
-            current: previewUrl, // 当前显示图片的http链接
-            urls: [previewUrl] // 需要预览的图片http链接列表
-        });
-    });
-
     /**
      * 显示语音布局
      */
