4b638c0578e8f1066dab76bcb41f4c883c2961ed
zs <1111>
2017-07-25 09:08:36 +0800

    login_t

--- src/main/webapp/common/js/ajax.js
+++ src/main/webapp/common/js/ajax.js
@@ -73,7 +73,7 @@ var url = {
     'w_token':url_o+'/wechatSignature/getWechatToken.do',
     'w_openId':url_o+'teacherAnalysis/queryStudentWechat.do',
     'w_push':url_o+'wechatSignature/sendTemplateMsg.do',//微信推送成绩信息
-    'w_email':url_o+'e2Login/queryTeacherInfo.do'
+    'w_email':url_o+'e2Login/queryTeacherInfo.do'//查询老师信息
 };
 var Study = {
     's_study':url_o+'studentAnalysis/scoreStdIdlReportStatus.do',  //学生获取个人成绩
