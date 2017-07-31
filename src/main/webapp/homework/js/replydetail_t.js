16215033a52973a7c99cf851d4726e35cdf2a93e
v_gaowensehng <6-FMX9j&cf>
2017-07-31 10:17:15 +0800

    提交

--- src/main/webapp/homework/js/replydetail_t.js
+++ src/main/webapp/homework/js/replydetail_t.js
@@ -257,10 +257,10 @@ $(function () {
             $('.areyok input:last-of-type').css('background', '#00ba97');
             return false;
         }
-        if ($('.teBox').html() == '') {
+        if ($('.teBox').val() == '') {
             need.replyDesc = '';
         } else {
-            need.replyDesc = $('.teBox').html();
+            need.replyDesc = $('.teBox').val();
         }
         if ($('.infoTitle span').css('color') == 'rgb(255, 106, 106)') {
             need.tag = '0'
@@ -277,11 +277,15 @@ $(function () {
                     }else{
                         need.replyDesc += encodeURI($('.anDes').eq(o).html()+'|>|');
                     }
+                    need.replyTimes = +'1'
                 }
             }
         }else{
             need.replyDesc = encodeURI($('.answer textarea').val()+'|>|');
+            need.replyTimes = '1'
+
         }
+
         ajax_S(homework_s.t_succ, need, function (e) {
             if (e.result == true) {
                 $('.big_back').show();
