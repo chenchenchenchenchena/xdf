565124d5bb84976a97f698ab5d98e433df3ade53
v_gaowensehng <6-FMX9j&cf>
2017-07-31 17:44:10 +0800

    提交

--- src/main/webapp/homework/js/replydetail_t.js
+++ src/main/webapp/homework/js/replydetail_t.js
@@ -78,7 +78,7 @@ $(function () {
 
             var arr = decodeURI(e.data.replyDesc).split('|>|');
             for(var L=0;L<arr.length;L++){
-                if (arr[L] != ''&&arr[L]!=undefined) {
+                if (arr[L] != ''&&arr[L]!=undefined&&arr[L]!=null) {
                         $('.tea_sp').append('<div class="hmAnswer"><div class="infoTitle">老师批复 </div><div class="anDes">' + arr[L] + '</div><div><ul class="voiceBox" id="audio_3"></ul><div class="imgBox"></div></div></div>')
                     }else {
                     $('.tea_sp').append('<div class="hmAnswer"><div class="infoTitle">老师批复 </div><div><ul class="voiceBox" id="audio_3"></ul><div class="imgBox"></div></div></div>');
