3033a0d9338b6dcb82fd6304316988f8f8e22a24
v_gaowensehng <6-FMX9j&cf>
2017-07-31 14:25:52 +0800

    提交

--- src/main/webapp/homework/js/replydetail_t.js
+++ src/main/webapp/homework/js/replydetail_t.js
@@ -722,7 +722,7 @@ $(function () {
         $(document.body).on('touchmove', function(event) {
             var y = event.originalEvent.changedTouches[0].clientY;
             var st = $(this).scrollTop(); //滚动条高度
-            if (y >= lastY && st <= 10) {//如果滚动条高度小于0，可以理解为到顶了，且是下拉情况下，阻止touchmove事件。
+            if (y >= lastY && st <= 1) {//如果滚动条高度小于0，可以理解为到顶了，且是下拉情况下，阻止touchmove事件。
                 lastY = y;
                 event.preventDefault();
             }
