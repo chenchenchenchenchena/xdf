e0fec6cb7171a79ea277c143d93a4783064852d8
v_liwei8 <v_liwei8@xdf.cn>
2017-08-04 16:08:51 +0800

    老师课表查看学情 添加样式

--- src/main/webapp/schedule/js/studentList.js
+++ src/main/webapp/schedule/js/studentList.js
@@ -1,14 +1,16 @@
 $(function(){
 	var flag=true;
 	$(".studentList").hide();
-	$(".slist .listCon").tap(function(event){
+	$(".slist .listCon .teClass").tap(function(event){
 		if($(".studentList").eq($(this).index()).css("display")=="none"){
 			$(".arrow").eq($(this).index()).css("background","url(images/upArrow.png)");
+			$(".studentList").eq($(this).index()).show();
 		}else{
 			$(".arrow").eq($(this).index()).css("background","url(images/belowArrow.png)");
+			$(".studentList").eq($(this).index()).hide();
 		}
 		$(".arrow").eq($(this).index()).css("background-size","0.31rem 0.17rem")
-		$(".studentList").eq($(this).index()).toggle();
+		// $(".studentList").eq($(this).index()).toggle();
         event.stopPropagation();
 	})
 })
