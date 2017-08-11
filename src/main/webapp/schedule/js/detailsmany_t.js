d85a1c37e117fff740d7b17947f704f3f927dc6f
v_gaowensehng <6-FMX9j&cf>
2017-08-11 17:46:00 +0800

    提交

--- src/main/webapp/schedule/js/detailsmany_t.js
+++ src/main/webapp/schedule/js/detailsmany_t.js
@@ -49,6 +49,7 @@ $(function () {
         } else {
             var begintime = '';
         }
+        
         if (BeginDate[timeindex].SectEnd != undefined && BeginDate[timeindex].SectEnd != null && BeginDate[timeindex].SectEnd != '') {
             var endtime = BeginDate[timeindex].SectEnd.split(' ')[1].substring(0, BeginDate[timeindex].SectEnd.split(' ')[1].length - 3)
         } else {
