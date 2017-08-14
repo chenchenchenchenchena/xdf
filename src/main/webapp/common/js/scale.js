25d4b02b9c7bf9cf1756d5f26493406e9dd41fa2
v_gaowensehng <6-FMX9j&cf>
2017-08-14 15:36:24 +0800

    部分入口修改

--- src/main/webapp/common/js/scale.js
+++ src/main/webapp/common/js/scale.js
@@ -2,7 +2,10 @@
 
 
 if(/Android (\d+\.\d+)/.test(navigator.userAgent)){
+    alert(0)
     var version = parseFloat(RegExp.$1);
+    alert(version)
+
     if(version>2.3){
         var phoneScale = parseInt(window.screen.width)/750;
         document.write('<meta name="viewport" content="width=750, minimum-scale = '+ phoneScale +', maximum-scale = '+ phoneScale +', target-densitydpi=device-dpi">');
