dbe9bd17797660dd26f6a7011de928676b563f35
v_gaowensehng <6-FMX9j&cf>
2017-08-17 15:30:06 +0800

    修改

--- src/main/webapp/learningSituation/js/common_ts.js
+++ src/main/webapp/learningSituation/js/common_ts.js
@@ -12,7 +12,7 @@ $(function(){
             for(var i = 0;i<more_.length;i++){
                 more_[i].classCorrectRate = parseInt(parseFloat(more_[i].classCorrectRate)*100);
                 more_[i].perCorrectRate = parseInt(parseFloat(more_[i].perCorrectRate)*100);
-                $('.common_yh').append('<div class="less_yh"><div class="less_yh_tit"><h4>第'+more_[i].lessonNo+'课次<span>'+more_[i].lessonStart+'-<i>'+more_[i].lessonEnd.substr(more_[i].lessonEnd.length-5,more_[i].lessonEnd.length)+'</i></span></h4></div><ul class="less_number"><li><h4>'+more_[i].perAnswerCount+' <span> / '+more_[i].allCount+'</span></h4><p>答题量/提问量</p></li><li><h4>'+more_[i].classCorrectRate+'% <span> / '+more_[i].perCorrectRate+' %</span></h4><p>正确率/班均正确率</p></li><li><h4>'+more_[i].perAverageTime+'"    <span> / '+more_[i].classAverageTime+'"</span></h4><p>均用时/班级均用时</p></li></ul></div>')
+                $('.common_yh').append('<div class="less_yh"><div class="less_yh_tit"><h4>第'+more_[i].lessonNo+'课次<span>'+more_[i].lessonStart+'-<i>'+more_[i].lessonEnd.substr(more_[i].lessonEnd.length-5,more_[i].lessonEnd.length)+'</i></span></h4></div><ul class="less_number"><li><h4>'+more_[i].perAnswerCount+' <span> / '+more_[i].allCount+'</span></h4><p>答题量/提问量</p></li><li><h4>'+more_[i].perCorrectRate+'% <span> / '+more_[i].classCorrectRate+' %</span></h4><p>正确率/班均正确率</p></li><li><h4>'+more_[i].perAverageTime+'"    <span> / '+more_[i].classAverageTime+'"</span></h4><p>均用时/班级均用时</p></li></ul></div>')
 
             }
         }else{
