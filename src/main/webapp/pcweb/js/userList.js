aa81c787a32e7e3be795ce7c3c2a6eb3726c9cb0
v_liwei8 <v_liwei8@xdf.cn>
2017-08-22 18:04:42 +0800

    pcweb 账号学校接口

--- src/main/webapp/pcweb/js/userList.js
+++ src/main/webapp/pcweb/js/userList.js
@@ -8,13 +8,33 @@ var table = {
         "tableName": "dict_school_info"
     };
 ajaxRequest("POST", url.s_select, table, selectData);
-function selectData(e) {
-    console.log(e);
-    $(".select ul").html("");
-    if (e.code == "200") {
-        // for (var i = 0; i < e.data.length; i++) {
-        //     var str = '<li data-value=' + e.data[i].tName + ' data-code=' + e.data[i].tCode + '>' + e.data[i].tName + '</li>';
-        //     $(".select ul").append(str);
-        // }
+function selectData(json) {
+    console.log(json);
+    // $(".select ul").html("");
+    // if (e.code == "200") {
+    // for (var i = 0; i < e.data.length; i++) {
+    //     var str = '<li data-value=' + e.data[i].tName + ' data-code=' + e.data[i].tCode + '>' + e.data[i].tName + '</li>';
+    //     $(".select ul").append(str);
+    // }
+
+    // }
+    if (json.code == "200") {
+        var schoolList = json.data;
+        var cityContent = "<a href='#' onclick='filterByCityId(this, \"\")' >全部</a>";
+        //var cityContent = "";
+        for (var i = 0; i < schoolList.length; i++) {
+            var schoolId = schoolList[i].tCode;
+            if (schoolId == "1") {
+                cityContent += "<a href='# 'class='cur' " +
+                    " >" + schoolList[i].tName + "</a>";
+            } else {
+                cityContent += "<a href='#' " +
+                    " >" + schoolList[i].tName + "</a>";
+            }
+        }
+        $(".xian-wid").html(cityContent);
+        // initOpenAndShou1();
+    } else {
+        layer.msg("查询失败!", {icon: 5})
     }
 }
