9b99cbca983b12c7aa38ff03db76b06f74f3428a
v_liwei8 <v_liwei8@xdf.cn>
2017-09-11 17:10:38 +0800

    学生主页UI布局调整

--- src/main/webapp/homework/js/rankinglistE.js
+++ src/main/webapp/homework/js/rankinglistE.js
@@ -18,4 +18,46 @@ $(function () {
         $('.title_s p').eq(1).html(data.teacherName + '老师');
         $('.title_s p').eq(2).html('日期:' + data.homeworkTime);
     });
+    /*** 测试数据*/
+    //链接到分享页
+    var checkStuArry = [];// 传递选中学生号
+    $(document).on('touchstart','.shareBtn',function () {
+        if($('.ranklibe li .check-box').length<=0){
+            layer.msg("请选择要分享的学员");
+            return ;
+        }
+        $('.ranklibe li .check-box').each(function () {
+            checkStuArry.push($(this).attr('data-stuNo'));
+            console.log(checkStuArry);
+        });
+        localStorage.studentNos = JSON.stringify({'checkStuArry':checkStuArry});
+        $('.tab-title,.main-content,.no-data').hide();
+        var testState = $('.main-content').attr('testState');
+        window.location.href = "sharedranking_t.html?testState="+testState+"&checkedTypeName="+checkedTypeName;
+    });
+    // 全选
+    var checkAll = true;//默认全选
+    $(document).on('touchstart','.check-tr',function () {
+        if(checkAll){
+            $('.ranklibe li .check-box').addClass('checked');
+            checkAll = false;
+        }else{
+            $('.ranklibe li .check-box').removeClass('checked');
+            checkAll = true;
+        }
+    });
+    // 多选
+    $(document).on('touchstart','.intro-test tr .check-td',function () {
+        $(this).find('.check-box').toggleClass('checked');
+        if(!$(this).find('.check-box').hasClass('checked')){
+            $('.check-tr .check-box').removeClass('checked');
+            checkAll = true;
+        }else{
+            if($('.intro-test tr td .checked').length== $('.intro-test tr td .check-box').length){
+                $('.intro-test .check-box').addClass('checked');
+                checkAll = false;
+            }
+        }
+    });
+
 })
