6821ea8fe502ba8b81b1056365dbcc45b2696d3c
v_gaowensehng <6-FMX9j&cf>
2017-09-11 16:18:15 +0800

    修改登录

--- src/main/webapp/schedule/js/login_t.js
+++ src/main/webapp/schedule/js/login_t.js
@@ -1,5 +1,6 @@
 //sessionStorage.openid = 'ofZfFwgizCmzR5XXMQtC5Wx5wZrA'
 // sessionStorage.stuNum= 'sy1';
+sessionStorage.openid = 'abcdfg'
 var WXnum  = {
     'wechatId':sessionStorage.openid
 };
