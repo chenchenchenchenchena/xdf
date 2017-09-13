b155e1b007222b15b9187defb9f42a4853e3bcbf
v_gaowensehng <6-FMX9j&cf>
2017-09-13 11:51:42 +0800

    更改

--- src/main/webapp/schedule/js/login_s.js
+++ src/main/webapp/schedule/js/login_s.js
@@ -4,7 +4,6 @@ $(function(){
     // if(!sessionStorage.openid){
     //     sessionStorage.openid = 'abcd123'
     // }
-    debugger;
     if(!sessionStorage.openid){
         wechatCode(location.href)
     }
