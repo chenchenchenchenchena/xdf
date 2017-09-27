23f3d3c71a5caaab615edc6c2fee05825c4c5ad8
v_liwei8 <v_liwei8@xdf.cn>
2017-09-27 18:06:16 +0800

    文本加密

--- src/main/webapp/learningSituation/js/rankList.js
+++ src/main/webapp/learningSituation/js/rankList.js
@@ -63,7 +63,12 @@ $(function(){
         sessionStorage.lessonNo = JSON.stringify(checkedLesson);
         $('.tab-title,.main-content,.no-data').hide();
         var testState = $('.main-content').attr('testState');
-        window.location.href = "sharedranking_t.html?testState=" + testState + "&checkedTypeName=" + checkedTypeName+"&type=student";
+        if(checkedLesson.length == 0){
+            window.location.href = "sharedranking_t.html?testState=" + testState + "&checkedTypeName=" + checkedTypeName+"&type=";
+        }else {
+            window.location.href = "sharedranking_t.html?testState=" + testState + "&checkedTypeName=" + checkedTypeName+"&type=student";
+        }
+
     });
     // 全选
     var checkAll = true;//默认全选
