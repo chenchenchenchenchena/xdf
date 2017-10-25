eedf588c08f9ae36dbf712c5f31731dba0d7aee1
xupingwei <137939412@qq.com>
2017-10-25 17:47:10 +0800

    新建主讲管理页

--- src/main/webapp/pcweb/accountManager/js/master.js
+++ src/main/webapp/pcweb/accountManager/js/master.js
@@ -19,11 +19,16 @@ $(function () {
                     var gradeCourse = userList[i].gradeCourse;
                     var isEnabled = userList[i].invalid;
 
+                    if(gradeCourse == undefined){
+                        gradeCourse = "";
+                    }
+
                     if (i % 2 == 1) {
                         str += "<tr class='table-tr-odd'>"
                     } else {
                         str += "<tr class='table-tr-even'>"
                     }
+                    str += "<td id='" + pid + "' style='display: none'>" + isEnabled + "</td>"
                     str += "<td id='" + pid + "' style='display: none'></td>";
                     str += "<td style='display: none'>" + pid + "</td>";
                     str += "<td>" + userName + "</td>";
@@ -54,14 +59,6 @@ $(function () {
         });
     }
 
-    /**
-     * 启用／禁用
-     * @param pid
-     */
-    function enabledUser(pid) {
-
-    }
-
 
     $('.false_l').click(function () {
         $('.back_big').hide();
@@ -83,15 +80,91 @@ $(function () {
             alert("科目不为空！")
             return;
         }
+        var params = {
+            'masterTeacherData':[
+                {
+                    'name':name,
+                    'email':email,
+                    'gradeCourse':subject
+                }
+            ]
+        }
+        var url = url_o + "backEndMasterTeacherManager/addMasterTeacher.do";
+        ajax_S(url, params, function (json) {
+            if(json.result){
+                $('.back_big').hide();
+                $('.add_l').hide();
+                layer.msg("添加成功!", {icon: 6});
+            }else {
 
-        // ajax_S(url.data_s, '', function (json) {
-        //     // if(json){
-        //     //
-        //     // }
-        // });
+                $('.back_big').hide();
+                $('.add_l').hide();
+                layer.msg("添加失败!", {icon: 6});
+                // alert(json.message);
+            }
+        });
     });
 
 })
+
+/**
+ * 启用／禁用
+ * @param pid
+ */
+function enabledUser(_this,pid) {
+    var isEnable = $("#" + pid).html();
+    var text;
+    var buttons = [];
+    if (isEnable == 1) {
+        text = "确定禁用该数据?";
+        buttons.push('禁用', '取消');
+    } else {
+        text = "确定启用该数据?";
+        buttons.push('启用', '取消')
+    }
+    layer.confirm(text, {
+        btn: buttons //按钮
+    }, function () {
+        var params = {
+            'id':pid,
+            'invalid':isEnable
+        };
+
+        jQuery.ajax({
+            type: "POST",
+            url: url_o + "backEndMasterTeacherManager/enabledMasterTeacher.do",
+            async: false,//同步
+            dataType: 'json',
+            data: JSON.stringify(params),
+            success: function (json) {
+                if (json.result == true) {
+                    if (isEnable == 1) {
+                        layer.msg("禁用成功!", {icon: 6});
+                        $(_this).html("<i></i>启用");
+                        $(_this).removeClass();
+                        $(_this).addClass('p176-btn-disable');
+                        $("#" + pid).html(0)
+                    } else {
+                        layer.msg("启用成功!", {icon: 6});
+                        $(_this).html("<i></i>禁用");
+                        $(_this).removeClass();
+                        $(_this).addClass('p176-btn-able');
+                        $("#" + pid).html(1)
+                    }
+                    //findList(1, currentCityId, currentAreaId, currentDeptId);
+                } else {
+                    if (isEnable == 1) {
+                        layer.msg("禁用失败!", {icon: 5});
+                    } else {
+                        layer.msg("启用失败!", {icon: 5});
+                    }
+                }
+            }
+        });
+    }, function () {
+
+    });
+}
 
 
 /**
