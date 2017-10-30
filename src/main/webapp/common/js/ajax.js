7455d09cbdd753a52955a94cda13118e92717647
v_gaowensehng <6-FMX9j&cf>
2017-10-30 16:35:46 +0800

    TIJIAO

--- src/main/webapp/common/js/ajax.js
+++ src/main/webapp/common/js/ajax.js
@@ -161,7 +161,7 @@ if(localStorage.mastTeater){
 //权限限制
 if(localStorage.terEmail&&!localStorage.mastTeater&&!localStorage.Assistant){
     // 获取主讲
-        ajax_S(url.data_s, '1', function (e) {
+        (url.data_s, '1', function (e) {
             for (var i = 0; i < e.data.length; i++) {
                 if(localStorage.terEmail==e.data[i].accountId){
                     localStorage.mastTeater = 'true'
@@ -327,9 +327,9 @@ function ajaxRequest(typeIn, targetUrl, requestData, successCallback) {
             };
             var a = new Date();
             var b = a.getTime()+1000*5;
-            sessionStorage.oldtime_D =  new Date(b).format("yyyy-MM-dd hh:mm:ss");
+            sessionStorage.oldtime_T =  new Date(b).format("yyyy-MM-dd hh:mm:ss");
             time_ = setInterval(function(){
-                if(sessionStorage.oldtime_D<new Date().format("yyyy-MM-dd hh:mm:ss")){
+                if(sessionStorage.oldtime_T<new Date().format("yyyy-MM-dd hh:mm:ss")){
                    $('body').append('<div style="position:fixed;top:0;left:0;width:100%;height:100%;z-index:20;background:#f1f1f1;"><img src="../common/images/erro_w.png" alt="" style="display:block;margin:335px auto 0;"><p style="display:block;width:213px;height:88px;background:#00ba97;color:#fff;border-radius:8px;text-align:center;font-size:32px;line-height:88px;margin:50px auto 0;" onclick="location.reload();">重新加载</p></div>')
                 }
             },1000)
