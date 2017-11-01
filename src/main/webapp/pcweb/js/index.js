45431321a6e04b2cafc2837ad57a15a86eaf9e3f
v_gaowensehng <6-FMX9j&cf>
2017-11-01 16:20:24 +0800

    css兼容

--- src/main/webapp/pcweb/js/index.js
+++ src/main/webapp/pcweb/js/index.js
@@ -1,4 +1,9 @@
 /* 概况 */
 
 
-
+/*css 兼容*/
+$('.index_forms li:nth-child(odd)').css('background','#f5fbfa')
+$('.index_forms li:first-of-type').css({
+    'background':'#fff',
+    'color':'#bababa'
+})
