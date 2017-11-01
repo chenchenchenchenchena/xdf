c6a481c6f41d0068febeff5cf2332479e6efe498
v_gaowensehng <6-FMX9j&cf>
2017-11-01 17:01:22 +0800

    d

--- src/main/webapp/pcweb/js/requireConfig.js
+++ src/main/webapp/pcweb/js/requireConfig.js
@@ -1,11 +1,21 @@
 /* 配置文件*/
 
 /*Life without love crt 2017-11-1 G*/
-
+require(['jquery-1.11.0.min'],function(){
 /*数据交互请求地址*/
-var global = {
-
-}
-
-
-
+    var global = {
+    
+    }
+    
+    
+    
+    /*用户下拉*/
+    
+    $('.user_name').hover(function(){
+        $('.usermenu').show();
+        // $('.user_name').css('background-images','url(../images/img_3.png)')
+    },function(){
+        $('.usermenu').hide();
+    })
+   
+})
