b52c120bf0f82628af8c0ae009c68a7ebe9fd639
v_gaowensehng <6-FMX9j&cf>
2017-11-02 09:21:36 +0800

    。。

--- src/main/webapp/pcweb/js/homework.js
+++ src/main/webapp/pcweb/js/homework.js
@@ -1,4 +1,5 @@
 /* 作业统计 */
 
 
-
+/*默认导航选中样式*/ 
+$('.left_nav li').eq(1).addClass('activ_nav')
--- src/main/webapp/pcweb/js/requireConfig.js
+++ src/main/webapp/pcweb/js/requireConfig.js
@@ -10,12 +10,21 @@ require(['jquery-1.11.0.min'],function(){
     
     
     /*用户下拉*/
-    
+    var timeout; 
     $('.user_name').hover(function(){
-        $('.usermenu').show();
-        // $('.user_name').css('background-images','url(../images/img_3.png)')
+        $('.usermenu').stop().fadeIn();
+        $('.user_name').css('background-image','url(../images/img_3.png)')
     },function(){
-        // $('.usermenu').hide();
+        timeout = setTimeout(function(){
+            $('.usermenu').stop().fadeOut();
+             $('.user_name').css('background-image','url(../images/img_1.png)')
+        },500)
     })
-   
+    $('.usermenu').hover(function(){
+        clearTimeout(timeout)
+    },function(){
+        $('.usermenu').stop().fadeOut();
+        $('.user_name').css('background-image','url(../images/img_1.png)')
+    })
+    
 })
