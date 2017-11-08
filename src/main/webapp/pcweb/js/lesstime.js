21894677c4dc46cf7fbdd0342ba8a4e7383f78c2
xupingwei <137939412@qq.com>
2017-11-08 16:16:17 +0800

    语音图片加载失败处理

--- src/main/webapp/pcweb/js/lesstime.js
+++ src/main/webapp/pcweb/js/lesstime.js
@@ -10,6 +10,7 @@ require(['jquery-1.11.0.min'], function () {
             endTime = $('.homewor_big_selecet .homework_end').val();
         }
 
+        SelectData();
         //$('.homewor_big_selecet .homework_begin').
     });
 });
@@ -87,12 +88,16 @@ function getDate(){
     }
 
 }
+
 function SelectData(){
+    beginTime = "2017-11-01";
+    endTime = "2017-11-01";
+    currentCityId = "73"
     if(beginTime == "" && endTime == ""){
         //layer.msg("请先选择日期");
         return false;
     }
-    var table = {
+    var params = {
         "schoolId": currentCityId,
         'beginDate':beginTime,
         'endDate':endTime
@@ -102,10 +107,27 @@ function SelectData(){
         url: url_o + 'backEndClassHourCount/queryClassHourCount.do',
         async: true,//同步
         dataType: 'json',
-        data: table,
+        data: JSON.stringify(params),
         success: function (e) {
-            sessionStorage.schoolList = JSON.stringify(e);
-            showSchoolList(e)
+            var data = e.Data;
+            if (data != undefined) {
+                var masterTeacherTotal = data.masterTeacherTotal;
+                var classTeacherTotal = data.classTeacherTotal;
+                $('#head_lesstime h1').html(classTeacherTotal);
+                $('#master_lesstime h1').html(masterTeacherTotal);
+                var list = data.data;
+                if(list != undefined && list.length > 0 ){
+                    $('.homework_list li').remove();
+
+                    var str_th = '<li class="homework_list_title"><span>校区</span><span>班主任数量（人）</span><span>班课量（个）</span><span>课时量（h）</span></li>';
+                    $('.homework_list').append(str_th);
+
+                    for (var i = 0; i < list.length; i++){
+                        var html_ = "<li><span>"+list[i].schoolName+"</span><span>"+list[i].headTeacherTotal+"</span><span>"+list[i].totalLessonNos+"</span><span>"+list[i].totalLessonHour+"</span></li>";
+                        $('.homework_list').append(html_);
+                    }
+                }
+            }
         }
     })
 }
