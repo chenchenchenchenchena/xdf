1b796c5fd76ce2216cb874ef68d6d3566ca7e8fb
v_gaowensehng <6-FMX9j&cf>
2017-11-13 18:18:32 +0800

    tijiao

--- src/main/webapp/pcweb/js/requireConfig.js
+++ src/main/webapp/pcweb/js/requireConfig.js
@@ -116,7 +116,7 @@ require(['jquery-1.11.0.min'],function(){
                         $('.left_nav ul .active_me').eq(0).addClass('activ_nav');
                     }
                     if(url_l.indexOf($bure_true.attr('href'))<0){
-                        alert(0)
+                        location.href =url_l.substr(0,url_l.indexOf('#'))+$url_;
                     }
 
                 }
