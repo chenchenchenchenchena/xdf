104d0ea1237f78d0eb36cd08ab9c11ddd23b8fc1
v_gaowensehng <6-FMX9j&cf>
2017-11-16 17:51:15 +0800

    tijiao

--- src/main/webapp/pcweb/js/homework.js
+++ src/main/webapp/pcweb/js/homework.js
@@ -495,9 +495,9 @@ function selectHwData() {
                         $('.normal').hide();
                         $('#correctRateAll').show();
                         $('#correctRateAll').css('float', 'left');
-                        commitAll = data.commitAll;//总提交数
-                        publishAll = data.publishAll;//总布置次数
-                        reachAll = data.reachAll;//总送达人次
+                        commitAll = parseInt(data.commitAll);//总提交数
+                        publishAll = parseInt(data.publishAll);//总布置次数
+                        reachAll = parseInt(data.reachAll);//总送达人次
                         var commitAllRate = parseInt(parseFloat((commitAll / reachAll)) * 100);
                         $('#publish h1 i').html(publishAll + "次");
                         $('#publish span').html("(总送达" + reachAll + "人次)");
