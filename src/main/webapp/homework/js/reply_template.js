56631913ab4bd65d50ab33f405054e84b5f680f9
xupingwei <137939412@qq.com>
2017-11-20 18:27:19 +0800

    添加模版页面

---
+++ src/main/webapp/homework/js/reply_template.js
@@ -0,0 +1,33 @@
+$(function(){
+    //滑动事件
+    $(document).on('touchstart mouusedown','.temp_list',function(){
+        // e.stopPropagation();
+        if($(this).children('.remove_temp')){
+            var begin_s = parseInt(event.targetTouches[0].pageX);
+            $(document).on('touchmove mousemove','.temp_list li',function(){
+                console.log($(this).offsetHeight());
+                var move_s = parseInt(event.targetTouches[0].pageX);
+                if(begin_s-move_s>=20){
+                    $(this).siblings().css('margin-left','0px');
+                    $(this).siblings().find('.remove_temp').css('right','-270px');
+                    $(this).css('margin-left','-181px');
+                    $(this).find('.remove_temp').css('right','-0px');
+                    $(this).parent().css('overflow','inherit');
+                    $(this).css('margin-left','-50px');
+                }else if(begin_s-move_s<=-20){
+                    $(this).css('margin-left','0px');
+                    $(this).find('.remove_temp').css('right','-270px');
+                    $(this).parent().css('overflow','hidden');
+                    $(this).css('margin-left','0');
+                }
+            });
+        }
+    });
+
+    /**
+     * 点击添加模版
+     */
+    $('.eBtn').click(function(){
+        location.href = 'add_template.html';
+    });
+})
