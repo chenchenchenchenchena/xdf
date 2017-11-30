e8e1a228137d1b23b0815a98c3ca3366303fedcd
v_gaowensehng <6-FMX9j&cf>
2017-11-30 17:07:21 +0800

    提交

--- src/main/webapp/pcweb/js/requireConfig.js
+++ src/main/webapp/pcweb/js/requireConfig.js
@@ -121,9 +121,11 @@ require(['jquery-1.11.0.min'],function(){
                     }else{
                         $('.left_nav ul .active_me').eq(0).addClass('activ_nav');
                     }
-                    if(url_l.indexOf($bure_true.attr('href'))<0){
+
+                    if(url_l.indexOf($bure_true.attr('href'))<0&&booleaN!=false){
                         location.href =url_l.substr(0,url_l.indexOf('#'))+$url_;
                     }
+
                     if(sessionStorage.userId!='ssdf'&&booleaN==false){
                         layer.msg('您暂无权限,请联系管理员');
                         $('.content').hide();
