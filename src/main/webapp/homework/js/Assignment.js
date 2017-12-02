65e7fe27f9f6c22c2f70650eb30de57b28e69d83
v_gaowensehng <6-FMX9j&cf>
2017-12-02 15:16:45 +0800

    提交

--- src/main/webapp/homework/js/Assignment.js
+++ src/main/webapp/homework/js/Assignment.js
@@ -21,7 +21,7 @@ $(function () {
     if(getRequest('state').state=='JT'||sessionStorage.signal){
         urlPush="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx67e16b7247bde1a8&redirect_uri=http://dt.xdf.cn/xdfdthome/homework/homeworklist_t.html&response_type=code&scope=snsapi_userinfo&state=JT&connect_redirect=1#wechat_redirect";
     }else{
-        urlPush="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxab29a3e2000b8d2a&redirect_uri=http://dt.xdf.cn/xdfdthome/homework/homeworklist_t.html&response_type=code&scope=snsapi_userinfo&state=JT&connect_redirect=1#wechat_redirect";
+        urlPush="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxab29a3e2000b8d2a&redirect_uri=http://dt.xdf.cn/xdfdthome/homework/homeworklist_t.html&response_type=code&scope=snsapi_userinfo&state=IQ&connect_redirect=1#wechat_redirect";
     }
     var trardata = {
         'schoolId': localStorage.schoolId,
