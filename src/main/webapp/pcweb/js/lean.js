a66d26b65f2a56f88f78b17f2c92f87ec2ec2997
xupingwei <137939412@qq.com>
2017-12-13 14:43:27 +0800

    pcweb 学情报告

---
+++ src/main/webapp/pcweb/js/lean.js
@@ -0,0 +1,172 @@
+
+/*默认筛选条件*/
+var currentSchoolId = "";//默认全部
+var currentSchool = "全部";//默认全部
+var beginTime = "";//默认全部
+var endTime = "";//默认全部
+var seacherName = "";
+var totalCounts = "0";
+var page = 1;
+var pageSize = 15;
+
+require(['jquery-1.11.0.min'], function () {
+    require(['jquery-ui.min'], function () {
+        require(['layer', 'requireConfig'], function () {
+            //$('.loading_pre').show();
+
+            laydate.render({
+                elem: '#date_input',
+                range: true //指定元素
+            });
+
+            //初始化分页控件
+            initPage(totalCounts, page);
+            //查询数据
+            SelectList();
+
+            //搜索点击事件
+            $('#seacher_hw').parent().find('img').click(function () {
+                $('.loading_pre').show();
+                SelectList();
+            });
+            //搜索回车事件
+            $('#seacher_hw').off("keyup").on('keyup',function(even){
+
+                if(even.keyCode==13){
+                    $('.loading_pre').show();
+                    $('.lesstime_Result').show();
+                    SelectList();
+                }
+            });
+            //筛选条件"确认"按钮 点击事件
+            $('#hw_selectBtn').click(function () {
+                $('.loading_pre').show();
+                $('.lesstime_Result').show();
+                SelectList();
+            });
+        });
+    });
+});
+
+
+//校区展示
+function getSchool() {
+    $('#select_html>ul>li').find('ul').hide();
+    if ($("#school").parent().find('ul').css('display') != 'none') {
+        $("#school").parent().find('ul').hide();
+    } else {
+        if (sessionStorage.schoolList) {
+            var json = JSON.parse(sessionStorage.schoolList);
+            showSchoolList(json);
+        } else {
+            var table = {
+                "tableName": "dict_school_info"
+            };
+            $.ajax({
+                type: "POST",
+                url: global.hw_dictList,
+                dataType: 'json',
+                data: table,
+                success: function (e) {
+                    sessionStorage.schoolList = JSON.stringify(e);
+                    showSchoolList(e)
+                }
+            })
+        }
+    }
+
+}
+
+//筛选校区列表显示
+function showSchoolList(e) {
+    var schoolStr = localStorage.schoolList;// 全部的校区ID
+    var schoolIdList = schoolStr.split(',');
+
+    var schoolList = e.data;
+    if (schoolList != undefined && schoolList.length > 0) {
+        $("#school").parent().find("ul").html("");
+        var cityContent = "<li onclick='filterByCityId(this, \"" + "全部" + "\")' data-schoolId='" + schoolStr + "'><span>全部</span></li>";
+        for (var i = 0; i < schoolIdList.length; i++) {
+            for (var j = 0; j < schoolList.length; j++) {
+                var schoolId = schoolList[j].tCode;
+                if (schoolIdList[i] == schoolId) {
+                    cityContent += "<li onclick='filterByCityId(this, \"" + schoolList[j].tName + "\")' data-schoolId='" + schoolId + "' ><span>" + schoolList[j].tName + "</span></li>";
+                }
+            }
+
+        }
+        $("#school").parent().find("ul").html(cityContent);
+    } else {
+        layer.msg("查询失败!")
+    }
+
+    $("#school").parent().find("ul").show();
+}
+
+//点击选择校区
+function filterByCityId(_this, schoolName) {
+    currentSchool = schoolName;
+    currentSchoolId = $(_this).attr('data-schoolId');
+    $("#school").html(currentSchool);
+    $("#school").parent().find("ul").hide();
+}
+
+function initPage(totalCounts, currentPage) {
+    if (totalCounts != null && totalCounts != 0) {
+        $.jqPaginator("#publicPage", {
+            totalCounts: totalCounts,
+            pageSize: pageSize,
+            visiblePages: pageSize,
+            currentPage: currentPage,
+            prev: '<a class="pPrev" href="javascript:;">上一页</a>',
+            next: '<a class="pNext" href="javascript:;">下一页</a>',
+            page: '<a href="javascript:;">{{page}}</a>',
+            activeClass: 'pCurrent',
+            onPageChange: function (num, type) {
+                if (type != "init") {
+                    page = num;
+                    SelectTeacherList();
+                }
+            }
+        });
+    } else {
+        $("#publicPage").html("");
+    }
+}
+
+function SelectList(){
+    //$('.loading_pre').show();
+
+    //获取筛选条件
+    var time = $('#date_input').val();
+    if (time != "" && time != undefined) {
+        beginTime = time.substring(0, 10);
+        endTime = time.substring(13, time.length);
+    }
+
+    seacherName = $('#seacher_hw').val();
+    if (seacherName == undefined) {
+        seacherName = "";
+    }
+
+    var params = {
+        'schoolId': currentSchoolId,
+        'teacher': seacherName,
+        'pageNum': page,
+        'pageSize': pageSize,
+        'beginTime': beginTime,
+        'endTime': endTime,
+        'homeworkType': homeworkType
+    };
+    //$.ajax({
+    //    type: "POST",
+    //    url: global.hw_details,
+    //    dataType: 'json',
+    //    contentType: "application/json",
+    //    data: JSON.stringify(params),
+    //    success: function (e) {
+    //
+    //    }
+    //})
+    $('.loading_pre').hide();
+}
