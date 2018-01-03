fba8c109cd8c2c19e776947744a4670d04741dc7
xupingwei <137939412@qq.com>
2018-01-03 17:19:18 +0800

    语音连续播放

--- src/main/webapp/homework/js/voice_check.js
+++ src/main/webapp/homework/js/voice_check.js
@@ -98,6 +98,15 @@ $(function () {
         if(isPlaying){
            return false;
         }
+        wx.config({
+            // 配置信息, 即使不正确也能使用 wx.ready
+            //debug: false,
+            //appId: '',
+            //timestamp: 1,
+            //nonceStr: '',
+            //signature: '',
+            //jsApiList: []
+        });
         wx.ready(function () {
             layer.msg($(audioCur).find('source').attr("src"));
             audioCur.play();
